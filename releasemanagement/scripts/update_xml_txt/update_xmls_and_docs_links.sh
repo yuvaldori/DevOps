a4ae928fec6cbc26b25d7248caa9d8113aaabb27
unknown <limor@limor-pc.gspaces.com>
2013-11-07 10:56:20 +0200

    update xmls and docs links script

---
+++ releasemanagement/scripts/update_xml_txt/update_xmls_and_docs_links.sh
@@ -0,0 +1,54 @@
+LOCAL_TRUNK_DIR=/tmp/update_xml_docs
+#LOCAL_TRUNK_DIR=/d/tmp/update_xml_docs
+LOG_FILE=$LOCAL_TRUNK_DIR/update_xml_docs.log
+
+#echo "*** start ***" > $LOG_FILE
+
+mkdir -p $LOCAL_TRUNK_DIR
+
+
+#openspaces-jetty, openspaces-scala, mule - only xml gs-webui - only wiki space
+dirs="examples gigaspaces gigaspaces-dotnet gigaspaces-poco/cpp gs-webui mule openspaces openspaces-jetty openspaces-scala quality/frameworks/QA"
+
+for dir in $dirs
+do
+	echo "*** checkout $LOCAL_TRUNK_DIR/$dir ***" >> $LOG_FILE
+	svn checkout svn://svn-srv/SVN/xap/trunk/$dir $LOCAL_TRUNK_DIR/$dir
+done
+
+echo "*** Update XMLs with new version of XSDs and DTD ***" >> $LOG_FILE
+find "${LOCAL_TRUNK_DIR}" -type f -not \( -name .svn -a -prune \) -name '*.xml' |
+while read fname
+do
+	grep "dtd\/9_7" "$fname" && echo "$fname" >> $LOG_FILE
+	sed -i 's/dtd\/9_7/dtd\/9_8/g' "$fname"
+	grep "schema\/9.7" "$fname" && echo "$fname" >> $LOG_FILE
+	sed -i 's/schema\/9.7/schema\/9.8/g' "$fname"
+done
+
+echo "*** Update Documentation links with new wiki space ***" >> $LOG_FILE
+find "${LOCAL_TRUNK_DIR}" -type f -not \( -name .svn -a -prune \) -name '*.html' -o -name '*.txt' -o -name '*.properties' -o -name '*.bat' -o -name '*.java' |
+while read fname
+do	
+	grep "display\/XAP97" "$fname" && echo "$fname" >> $LOG_FILE
+	sed -i 's/display\/XAP97/display\XAP98/g' "$fname"
+done
+
+echo "*** Display svn diff ***" >> $LOG_FILE
+for dir in $dirs
+do
+	echo "*** svn diff $LOCAL_TRUNK_DIR/$dir ***" >> $LOG_FILE
+	svn diff $LOCAL_TRUNK_DIR/$dir >> $LOG_FILE
+done
+
+#echo "*** Check-in changes to svn ***" >> $LOG_FILE
+#for dir in $dirs
+#do
+#	svn ci --username '' --password '' -m 'Update files with new version of XSDs, DTD and wiki space' $dir  
+#done
+
+
+
+
+
+
