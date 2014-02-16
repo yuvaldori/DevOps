41d13657a7a8fc3945a7b75df1257a744aac2624
limor-gs <limor@gigaspaces.com>
2014-02-16 14:35:33 +0200

    remove bigdata

--- releasemanagement/scripts/upload_release/copy_release_packages_to_website.sh
+++ releasemanagement/scripts/upload_release/copy_release_packages_to_website.sh
@@ -9,7 +9,8 @@ BUILD_DIR=${REPOSITORY}/${VERSION}/build_${BUILD_NUMBER}
 
 ssh  -i ~/.ssh/website  tempfiles@www.gigaspaces.com "mkdir -p ~/download_files/${MAJOR_VERSION}/${VERSION}/"
 
-for file in `find ${BUILD_DIR} -name "*.zip" -o -name "*.tar.gz" -o -name "*.msi"  | grep -v license | grep -v testsuite | grep -v xap-premium.1.5`; do
+#for file in `find ${BUILD_DIR} -name "*.zip" -o -name "*.tar.gz" -o -name "*.msi"  | grep -v license | grep -v testsuite | grep -v xap-premium.1.5`; do
+for file in `find ${BUILD_DIR} -name "*.zip" -o -name "*.tar.gz" -o -name "*.msi"  | grep -v license | grep -v testsuite`; do
   echo Uploading $file to ~/download_files/${MAJOR_VERSION}/${VERSION}/
   scp -i ~/.ssh/website $file tempfiles@www.gigaspaces.com:~/download_files/${MAJOR_VERSION}/${VERSION}/
 done
