c38d140845bb7d85e928007b8da1b3037e409af8
limor-gs <limor@gigaspaces.com>
2014-04-03 09:46:25 +0300

    adding copy package to s3

--- releasemanagement/scripts/cosmo/upload_package.py
+++ releasemanagement/scripts/cosmo/upload_package.py
@@ -22,6 +22,8 @@ from pkg import *  # NOQA
 import config
 from packager import *  # NOQA
 import glob
+import params
+from boto.s3.connection import S3Connection
 
 
 
@@ -35,15 +37,28 @@ def copy_dir(src,dst):
         shutil.rmtree(dst)
     shutil.copytree(src, dst)
 
-
+#export TARZAN_BUILDS=/export/builds/cloudify3
 
 ## copy cloudify3 package...
-if glob.glob('{0}/cloudify*.deb'.format(config.PACKAGES['cloudify3']['package_path'])):
-	print "yes"
-	print os.environ["TARZAN_BUILDS"]	
-	copy_dir('{0}'.format(config.PACKAGES['cloudify3']['package_path']),os.environ["TARZAN_BUILDS"]+"/3.0.0")
+print "uploading cloudify3 package to s3 and tarzan/builds"
+PACKAGE_SOURCE_PATH='{0}'.format(config.PACKAGES['cloudify3']['package_path'])
+PACKAGE_DEST_PATH="latest"
+if glob.glob('{0}/cloudify*.deb'.format(PACKAGE_SOURCE_PATH)):
+
+	#print os.environ["TARZAN_BUILDS"]	
+	copy_dir('{0}'.format(PACKAGE_SOURCE_PATH),os.environ["TARZAN_BUILDS"]+"/"+PACKAGE_DEST_PATH)
+	print "uploaded file to {0}".format(os.environ["TARZAN_BUILDS"]+"/"+PACKAGE_DEST_PATH) 
+	os.chdir( PACKAGE_SOURCE_PATH ) 
+	filenames = ['cloudify3_3.0.0_amd64.deb', 'cloudify3-components_3.0.0_amd64.deb']
+	conn = S3Connection(aws_access_key_id=params.AWS_KEY, aws_secret_access_key=params.AWS_SECRET)
+	for fname in filenames:
+    		bucket = conn.get_bucket("gigaspaces-cosmo-packages")    		
+    		full_key_name = os.path.join(PACKAGE_DEST_PATH, fname)   	 	
+		key = bucket.new_key(full_key_name).set_contents_from_filename(fname, policy='public-read') 		
+   	 	print "uploaded file %s" % fname
+
 else:
-	print "Cannot upload cloudify3 to TARZAN because cloudify deb file is missing"
+	print "Cannot upload cloudify3 package because cloudify3 deb file is missing"
 	sys.exit(1)
 	
 
