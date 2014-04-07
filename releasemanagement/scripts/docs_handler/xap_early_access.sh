43200f63cbe4200891d7796b9f530ac2666ca322
limor-gs <limor@gigaspaces.com>
2014-04-07 18:12:40 +0300

    adding mongo-datasource-docs

--- releasemanagement/scripts/docs_handler/xap_early_access.sh
+++ releasemanagement/scripts/docs_handler/xap_early_access.sh
@@ -17,12 +17,14 @@ JAVADOC_LINK=JavaDoc${API_DOC_VERSION}
 DOTNETDOC_LINK=dotnetdocs${API_DOC_VERSION}
 CPPDOC_LINK=cppdocs${API_DOC_VERSION}
 SCALADOC_LINK=scaladocs${API_DOC_VERSION}
+MONGOEDSDOC_LINK=mongoeds-docs${API_DOC_VERSION}
 
 # DOCS DIRS
 JAVA_DOCS_DIR=${XAP_DIR}/docs/xap-javadoc 
 DOTNET_DOCS_DIR=${XAP_DIR}/dotnet/docs
 CPP_DOCS_DIR=${XAP_DIR}/cpp/docs/html
 SCALA_DOCS_DIR=${XAP_DIR}/docs/scaladocs
+MONGOEDS_DOCS_DIR=${XAP_DIR}/docs/mongoeds-docs
 
 cp ${XAP_EA_DIR}/${XAP_RELEASE_VERSION}/${MILESTONE}/${XAP_ZIP_FILENAME} ${DOC_DIR}
 
@@ -51,14 +53,20 @@ pushd ${DOC_DIR}
 	###### SCALA API ######	
 	unzip -d ${XAP_DIR}/docs ${XAP_DIR}/docs/openspaces-scala-scaladocs.zip
 
+	###### MONGOEDS API #####
+	mkdir  ${MONGOEDS_DOCS_DIR}	
+	unzip -d ${MONGOEDS_DOCS_DIR} ${XAP_DIR}/docs/mongo-datasource.zip
+
 	###### REMOVE LINKS ######
-	rm ${JAVADOC_LINK} ${DOTNETDOC_LINK} ${CPPDOC_LINK} ${SCALADOC_LINK}
+	rm ${JAVADOC_LINK} ${DOTNETDOC_LINK} ${CPPDOC_LINK} ${SCALADOC_LINK} ${MONGOEDSDOC_LINK}
 
 	###### CREATE LINKS ######
 	ln -s ${JAVA_DOCS_DIR} ${JAVADOC_LINK}
 	ln -s ${DOTNET_DOCS_DIR} ${DOTNETDOC_LINK}
 	ln -s ${CPP_DOCS_DIR} ${CPPDOC_LINK}
 	ln -s ${SCALA_DOCS_DIR} ${SCALADOC_LINK}
+	ln -s ${MONGOEDS_DOCS_DIR} ${MONGOEDSDOC_LINK}
+
 
 popd
 
