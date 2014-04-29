de4b5dee1f51438c9010bc8a6ade0083ee61fa4d
limor-gs <limor@gigaspaces.com>
2014-04-29 19:01:32 +0300

    change upload file structure

--- releasemanagement/scripts/cosmo/continuous_integration.py
+++ releasemanagement/scripts/cosmo/continuous_integration.py
@@ -25,15 +25,20 @@ from packman import *  # NOQA
 #os.environ["PACK_COMPONENTS"]="yes" 
 #os.environ["PACK_CORE"]="yes" 
 #os.environ["PACK_UI"]="yes"
-#os.environ["BUILD_NUM"]="1-1"  
+#os.environ["BUILD_NUM"]="1-248" 
+#os.environ["PRODUCT_VERSION_FULL"]="3.0.0-m1-b1-248" 
+
 PACK_COMPONENTS=os.environ["PACK_COMPONENTS"]
 PACK_CORE=os.environ["PACK_CORE"]
 PACK_UI=os.environ["PACK_UI"]
 BUILD_NUM=os.environ["BUILD_NUM"]
+PRODUCT_VERSION=os.environ["PRODUCT_VERSION_FULL"]
+
 print PACK_COMPONENTS
 print PACK_CORE
 print PACK_UI
 print BUILD_NUM
+print PRODUCT_VERSION
 
 def copy_dir(src,dst):
     if os.path.exists(dst):
@@ -50,6 +55,13 @@ manager_conf = packages.PACKAGES['manager']
 celery_conf = packages.PACKAGES['celery']
 cloudify_ui_conf = packages.PACKAGES['cloudify-ui']
 linux_conf = packages.PACKAGES['linux-agent']
+cloudify_core_conf = packages.PACKAGES['cloudify-core']
+
+
+PACKAGE_SOURCE_PATH='{0}'.format(cloudify_core_conf['package_path'])
+if os.path.exists(PACKAGE_SOURCE_PATH):
+	local('sudo chown tgrid -R {0}'.format(PACKAGE_SOURCE_PATH),capture=False)
+	shutil.rmtree(PACKAGE_SOURCE_PATH)
 
 
 if PACK_CORE == "yes":
@@ -61,7 +73,9 @@ if PACK_CORE == "yes":
 	## install manager with dependencies into manager virtualenv
 	do('{0}/bin/pip --default-timeout=45 install {1}'.format(manager_conf['sources_path'],parent_dir+'/cloudify-manager/rest-service'))
 	## package manager virtualenv
-	pkg_manager()
+	packages.PACKAGES['manager']['version']=PRODUCT_VERSION
+	pack(packages.PACKAGES['manager'])
+	#pkg_manager()
 
 	print("*** packaging celery")
 	get_celery()
@@ -70,16 +84,23 @@ if PACK_CORE == "yes":
 	do('{0}/bin/pip --default-timeout=45 install {1}'.format(celery_conf['sources_path'],parent_dir+'/cloudify-plugins-common'))
 	do('{0}/bin/pip --default-timeout=45 install {1}'.format(celery_conf['sources_path'],parent_dir+'/cloudify-manager/plugins/plugin-installer'))
 	do('{0}/bin/pip --default-timeout=45 install {1}'.format(celery_conf['sources_path'],parent_dir+'/cloudify-manager/plugins/agent-installer'))
-	pkg_celery()
+	packages.PACKAGES['celery']['version']=PRODUCT_VERSION
+	pack(packages.PACKAGES['celery'])
+	#pkg_celery()
+
+	print("*** packaging cloudify-core")
 	manager_file = glob.glob(os.path.join('{0}'.format(manager_conf['package_path']), '{0}*.deb'.format(manager_conf['name'])))
 	print manager_file
 	celery_file = glob.glob(os.path.join('{0}'.format(celery_conf['package_path']), '{0}*.deb'.format(celery_conf['name'])))
 	print celery_file
 	if  manager_file and celery_file:
-		pkg_cloudify_core()
+		packages.PACKAGES['cloudify-core']['version']=PRODUCT_VERSION
+		pack(packages.PACKAGES['cloudify-core'])
+		#pkg_cloudify_core()
 	else:
 		print "Cannot pack cloudify-core because missing deb files"
 		sys.exit(1)
+	
 	print("*** packaging linux-agent")
 	get_linux_agent()
 	## install linux_agent with dependencies into celery virtualenv
@@ -87,22 +108,32 @@ if PACK_CORE == "yes":
 	do('{0}/bin/pip --default-timeout=45 install {1}'.format(linux_conf['sources_path'],parent_dir+'/cloudify-plugins-common'))
 	do('{0}/bin/pip --default-timeout=45 install {1}'.format(linux_conf['sources_path'],parent_dir+'/cloudify-manager/plugins/plugin-installer'))
 	do('{0}/bin/pip --default-timeout=45 install {1}'.format(linux_conf['sources_path'],parent_dir+'/cloudify-manager/plugins/agent-installer'))
-	pkg_linux_agent()
-	pkg_ubuntu_agent()
+	packages.PACKAGES['linux-agent']['version']=PRODUCT_VERSION
+	pack(packages.PACKAGES['linux-agent'])
+	packages.PACKAGES['ubuntu-agent']['version']=PRODUCT_VERSION
+	pack(packages.PACKAGES['ubuntu-agent'])
+	#pkg_linux_agent()
+	#pkg_ubuntu_agent()
 
 if PACK_UI == "yes":
 	print("*** packaging ui")
 	get_cloudify_ui()
-	shutil.copyfile(parent_dir+"/cosmo-ui/dist/cosmo-ui-1.0.0.tgz", "{0}/cosmo-ui-1.0.0.tgz".format(cloudify_ui_conf['sources_path']))
-	pkg_cloudify_ui()
+	#shutil.copyfile(parent_dir+"/cosmo-ui/dist/cosmo-ui-1.0.0.tgz", "{0}/cosmo-ui-1.0.0.tgz".format(cloudify_ui_conf['sources_path']))
+	#pkg_cloudify_ui()
 
-	ui_file = glob.glob(os.path.join('{0}'.format(cloudify_ui_conf['package_path']), '{0}*.deb'.format(cloudify_ui_conf['name'])))
-	print ui_file
+	tar_ui_file = glob.glob(os.path.join(parent_dir+'/cosmo-ui/dist','cosmo-ui*.tgz'))
+	print tar_ui_file
+	shutil.copy(''.join(tar_ui_file),'{0}'.format(cloudify_ui_conf['sources_path']))
+	
+	#deb_ui_file = glob.glob(os.path.join('{0}'.format(cloudify_ui_conf['package_path']), '{0}*.deb'.format(cloudify_ui_conf['name'])))
+	#print deb_ui_file
 
-	if  ui_file:
-		pkg_cloudify_ui()
+	if  tar_ui_file:
+		packages.PACKAGES['cloudify-ui']['version']=PRODUCT_VERSION
+		pack(packages.PACKAGES['cloudify-ui'])
+		#pkg_cloudify_ui()
 	else:
-		print "Cannot pack cloudify-ui because missing deb files"
+		print "Cannot pack cloudify-ui because missing tar files"
 		sys.exit(1)
 
 
--- releasemanagement/scripts/cosmo/upload_package.py
+++ releasemanagement/scripts/cosmo/upload_package.py
@@ -30,8 +30,11 @@ from boto.s3.connection import S3Connection
 #os.environ["PACK_COMPONENTS"]="yes" 
 #os.environ["PACK_CORE"]="yes" 
 #os.environ["PACK_UI"]="yes"
-#os.environ["BUILD_NUM"]="1-1"  
+#os.environ["BUILD_NUM"]="1-248"  
 #os.environ["CONFIGURATION_NAME"]="NightlyBuild" 
+#os.environ["PRODUCT_VERSION"]="3.0.0" 
+#os.environ["PRODUCT_VERSION_FULL"]="3.0.0-m1-b1-248" 
+#os.environ["CONFIGURATION_PATH_NAME"]="root/cosmo/trunk/CI/NightlyBuild"
 
 TARZAN_BUILDS=os.environ["TARZAN_BUILDS"] 
 PACK_COMPONENTS=os.environ["PACK_COMPONENTS"]
@@ -39,6 +42,9 @@ PACK_CORE=os.environ["PACK_CORE"]
 PACK_UI=os.environ["PACK_UI"]
 BUILD_NUM=os.environ["BUILD_NUM"]
 CONFIGURATION_NAME=os.environ["CONFIGURATION_NAME"]
+PRODUCT_VERSION=os.environ["PRODUCT_VERSION"]
+PRODUCT_VERSION_FULL=os.environ["PRODUCT_VERSION_FULL"]
+CONFIGURATION_PATH_NAME=os.environ["CONFIGURATION_PATH_NAME"]
 
 print TARZAN_BUILDS 
 print PACK_COMPONENTS
@@ -46,6 +52,9 @@ print PACK_CORE
 print PACK_UI
 print BUILD_NUM
 print CONFIGURATION_NAME
+print PRODUCT_VERSION
+print PRODUCT_VERSION_FULL
+print CONFIGURATION_PATH_NAME
 
 def mkdirp(directory):
     if not os.path.isdir(directory):
@@ -72,17 +81,22 @@ if CONFIGURATION_NAME == "NightlyBuild":
 	PACKAGE_DEST_DIR="nightly"
 else:
 	PACKAGE_DEST_DIR="continuous"
-PACKAGE_DEST_BUILD_DIR=PACKAGE_DEST_DIR+"_"+BUILD_NUM
+PACKAGE_DEST_BUILD_DIR=PRODUCT_VERSION+"/"+PACKAGE_DEST_DIR+"_"+BUILD_NUM
 PACKAGE_DEST_PATH="org/cloudify3/"+PACKAGE_DEST_DIR
 PACKAGE_DEST_BUILD_PATH="org/cloudify3/"+PACKAGE_DEST_BUILD_DIR
 
-print "check that all deb files exist in /cloudify folder"
+
+
+#commands.getoutput('sudo chown tgrid -R {0}'.format(PACKAGE_SOURCE_PATH))
+local('sudo chown tgrid -R {0}'.format(PACKAGE_SOURCE_PATH),capture=False)
 
 #This will be removed when the pkg_components will be ready
 if PACK_COMPONENTS == "yes":
 	print "copy 3rd parties deb from /packages folder"
-	shutil.copyfile('/packages/cloudify3-components_3.0.0_amd64.deb','{0}/cloudify-components_3.0.0_amd64.deb'.format(PACKAGE_SOURCE_PATH))
+	components_new_name='cloudify-components_'+PRODUCT_VERSION_FULL+'_amd64.deb'
+	shutil.copyfile('/packages/cloudify3-components_3.0.0_amd64.deb','{0}/{1}'.format(PACKAGE_SOURCE_PATH,components_new_name))
 
+print "check that all deb files exist in /cloudify folder"
 components_package = glob.glob('{0}/{1}*.deb'.format(PACKAGE_SOURCE_PATH,cloudify_components_conf['name']))
 print components_package
 core_package = glob.glob('{0}/{1}*.deb'.format(PACKAGE_SOURCE_PATH,cloudify_core_conf['name']))
@@ -116,29 +130,38 @@ if PACK_UI == "yes":
 		print "*** ui deb file is missing ***"
 print filenames
 
-#commands.getoutput('sudo chown tgrid -R {0}'.format(PACKAGE_SOURCE_PATH))
-local('sudo chown tgrid -R {0}'.format(PACKAGE_SOURCE_PATH),capture=False)
-
 
 print "uploading cloudify3 packages to s3 and tarzan/builds"
 
 
 os.chdir( PACKAGE_SOURCE_PATH )
 conn = S3Connection(aws_access_key_id=params.AWS_KEY, aws_secret_access_key=params.AWS_SECRET)
-for fname in filenames:				
-	mkdirp(TARZAN_BUILDS+"/"+PACKAGE_DEST_DIR)
-	shutil.copyfile(PACKAGE_SOURCE_PATH+"/"+fname,TARZAN_BUILDS+"/"+PACKAGE_DEST_DIR+"/"+fname)
+for fname in filenames:
+	if "trunk" in CONFIGURATION_PATH_NAME:				
+		mkdirp(TARZAN_BUILDS+"/"+PACKAGE_DEST_DIR)
+		#Removing the version from packge name for nightly and continuous folders
+		name_without_version=fname.replace(PRODUCT_VERSION_FULL+"_",'')
+		shutil.copyfile(PACKAGE_SOURCE_PATH+"/"+fname,TARZAN_BUILDS+"/"+PACKAGE_DEST_DIR+"/"+name_without_version)
+		f = open(TARZAN_BUILDS+'/'+PACKAGE_DEST_DIR+'/build_num', 'w')
+		f.write(BUILD_NUM)
 	
 	mkdirp(TARZAN_BUILDS+"/"+PACKAGE_DEST_BUILD_DIR)
 	shutil.copyfile(PACKAGE_SOURCE_PATH+"/"+fname,TARZAN_BUILDS+"/"+PACKAGE_DEST_BUILD_DIR+"/"+fname)
 
-    	bucket = conn.get_bucket("gigaspaces-repository-eu")  
+	
+	if "trunk" in CONFIGURATION_PATH_NAME:
+		bucket = conn.get_bucket("gigaspaces-repository-eu")
+		full_key_name = os.path.join(PACKAGE_DEST_PATH, name_without_version)   	 	
+		key = bucket.new_key(full_key_name).set_contents_from_filename(fname, policy='public-read') 		
+   		
+    	
+	bucket = conn.get_bucket("gigaspaces-repository-eu")
 	full_key_name = os.path.join(PACKAGE_DEST_BUILD_PATH, fname)   	 	
 	key = bucket.new_key(full_key_name).set_contents_from_filename(fname, policy='public-read')
+	
+	print "uploaded file %s" % fname
 	   		
-    	full_key_name = os.path.join(PACKAGE_DEST_PATH, fname)   	 	
-	key = bucket.new_key(full_key_name).set_contents_from_filename(fname, policy='public-read') 		
-   	print "uploaded file %s" % fname
+    	
 
 
 
