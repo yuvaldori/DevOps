1cbb5698bed184823c77132470e226911a37d4f9
limor-gs <limor@gigaspaces.com>
2014-05-20 18:08:49 +0300

    run integration tests trough docker lxc container

--- releasemanagement/scripts/cosmo/cosmo_integration_test.sh
+++ releasemanagement/scripts/cosmo/cosmo_integration_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source retry.sh
-	
+
 echo "### PATH is: $PATH"
 report_dir=`pwd`/xunit_reports
 
@@ -23,7 +23,38 @@ echo "### Installing nose"
 retry "pip install nose"
 
 echo "### Installing protobuf using easy_install"
-easy_install protobuf	
+easy_install protobuf
+
+REPOS_LIST="cloudify-dsl-parser cloudify-rest-client cloudify-plugins-common cloudify-cli \
+cloudify-manager/plugins/plugin-installer cloudify-manager/rest-service cloudify-bash-plugin"
+
+for r in ${REPOS_LIST}
+do
+     		
+	echo "### Processing repository: $r"	   	
+	
+	pushd $r
+
+		echo "### Installing [$r] dependencies"
+		if [ "$r" = "cloudify-plugins-common" ]
+		then
+			retry "python setup.py install"
+			retval=$?
+			if [ $retval -ne 0 ]; then
+				echo "### Installation for package [$r] exited with code $retval"
+				exit $retval
+			fi
+		else
+ 			retry "pip install . --process-dependency-links"
+			retval=$?
+			if [ $retval -ne 0 ]; then
+				echo "### Installation for package [$r] exited with code $retval"
+				exit $retval
+			fi
+		fi
+
+	popd
+done	
 
 pushd cloudify-manager
 
@@ -47,6 +78,11 @@ pushd cloudify-manager
 			exit $?
 		fi
 		echo "### Running integration tests"
+		/etc/init.d/rabbitmq-server start
+		export PATH=/usr/share/elasticsearch/bin:$PATH
+		ln -s /usr/local/bin/gunicorn /usr/bin/gunicorn
+		ln -s /usr/local/bin/gunicorn /usr/bin/gunicorn
+
 		nosetests -s -v workflow_tests --with-xunit --xunit-file=$report_dir/xunit-integration-tests.xml
 		retval=$?
 		echo "### Integration tests exited with code $retval"
---
+++ releasemanagement/scripts/cosmo/run_container_for_itests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function  exit_on_error {
+      status=$?
+      echo "exit code="$status    
+      if [ $status != 0 ] ; then
+            echo "Failed (exit code $status)"
+	     exit 1
+            
+      fi
+
+}
+echo "*** start a new container"
+
+sudo docker run -d cosmo_tests
+exit_on_error
+echo "*** get container ID"
+ID=`sudo docker ps -l | cut -d " " -f1 | tail -n +2 | tr -d ' '`
+exit_on_error
+echo "container ID="$ID
+echo "*** get container IP"
+IP=`sudo docker inspect $ID | grep IPAddress | cut -d ":" -f2 | cut -d '"' -f2 | tr -d ' '`
+exit_on_error
+echo "container IP="$IP
+
+#sudo ssh-keygen -f "/root/.ssh/known_hosts" -R $IP
+
+echo "*** copy nightly folder to the container"
+sudo scp -rp -i phusion.key cloudify-* root@$IP:/opt
+#exit_on_error
+sudo scp -p -i phusion.key * root@$IP:/opt
+#exit_on_error
+echo "*** run integration tests"
+echo "sudo ssh -i phusion.key root@$IP /opt/cosmo_integration_test.sh"
+sudo ssh -i phusion.key root@$IP cd /opt ; ./cosmo_integration_test.sh
+exit_on_error
+sudo docker stop $ID
+exit_on_error
+sudo docker rm $ID
+exit_on_error
