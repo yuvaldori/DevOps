48cd77f9bb44f0915dd1ed399072b87ca9e03c8b
limor-gs <limor@gigaspaces.com>
2014-06-30 09:24:00 +0300

    Update pack_linux64_cli.sh

--- releasemanagement/scripts/cosmo/pack_linux64_cli.sh
+++ releasemanagement/scripts/cosmo/pack_linux64_cli.sh
@@ -8,6 +8,17 @@
 
 source ../../cli_credentials.sh
 
+function  exit_on_error {
+      status=$?
+      echo "exit code="$status    
+      if [ $status != 0 ] ; then
+         	echo "Failed (exit code $status)" 
+		vagrant destroy -f windows            
+		exit 1
+      fi
+
+}
+
 rm -f /cloudify/cloudify-cli_*_amd64.deb
 
 
@@ -15,6 +26,7 @@ rm -f /cloudify/cloudify-cli_*_amd64.deb
 vagrant destroy -f linux64
 
 vagrant up linux64 --provider=aws
+exit_on_error
 
 #get guest ip address
 s=`vagrant ssh linux64 -- ec2metadata | grep public-hostname | cut -f1 -d"." | cut -d" " -f2` ; s=${s#ec2-} ; ip_address=${s//-/.}
@@ -23,6 +35,7 @@ echo "ip_address="$ip_address
 #copy linux64 deb file
 sudo chown tgrid -R /cloudify
 scp -i ~/.ssh/aws/vagrant_build.pem ubuntu@$ip_address:~/cloudify-cli-packager/pyinstaller/*.deb /cloudify
+exit_on_error
 
 vagrant destroy -f linux64
 
