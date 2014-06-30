03d71c6170a711a24ff883d7736bb45269830aed
limor-gs <limor@gigaspaces.com>
2014-06-30 12:47:24 +0300

    Update pack_components.sh

--- releasemanagement/scripts/cosmo/pack_components.sh
+++ releasemanagement/scripts/cosmo/pack_components.sh
@@ -8,6 +8,17 @@
 
 source ../../../cli_credentials.sh
 
+function  exit_on_error {
+      status=$?
+      echo "exit code="$status    
+      if [ $status != 0 ] ; then
+         	echo "Failed (exit code $status)" 
+		vagrant destroy -f
+		exit 1
+      fi
+
+}
+
 rm -f /cloudify/cloudify-components*_amd64.deb
 
 
@@ -15,6 +26,7 @@ rm -f /cloudify/cloudify-components*_amd64.deb
 vagrant destroy -f
 
 vagrant up --provider=aws
+exit_on_error
 
 #get guest ip address
 s=`vagrant ssh -- ec2metadata | grep public-hostname | cut -f1 -d"." | cut -d" " -f2` ; s=${s#ec2-} ; ip_address=${s//-/.}
@@ -24,5 +36,6 @@ echo "ip_address="$ip_address
 sudo mkdir -p /cloudify
 sudo chown tgrid -R /cloudify
 scp -i ~/.ssh/aws/vagrant_build.pem ubuntu@$ip_address:/cloudify-packager/output-packages/*.deb /cloudify
+exit_on_error
 
 vagrant destroy -f
