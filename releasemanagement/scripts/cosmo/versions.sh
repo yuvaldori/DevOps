983c84697459400cec833bd956e42e9ec94d9c80
opencm <opencm@gigaspaces.com>
2014-07-06 16:33:37 +0300

    Update and rename versions to versions.sh

--- releasemanagement/scripts/cosmo/versions.sh
+++ releasemanagement/scripts/cosmo/versions.sh
@@ -10,16 +10,16 @@ echo "### Repositories list: $REPOS_LIST"
 
 for r in ${REPOS_LIST}
 do
-
 	echo "### Processing repository: $r"	   	
 
 	pushd $r
-	  echo '{' > VERSION
-  	echo '    "version": "'$REVISION'",' >> VERSION
-  	echo '    "build": "'$BUILD_NUM'",' >> VERSION
-  	echo '    "date": "'`date +'%d/%m/%Y'`'",' >> VERSION
-  	echo '    "commit": "'$CONFIGURATION_NAME'"' >> VERSION
-  	echo '}' >> VERSION
+	  	echo '{' > VERSION
+  		echo '    "version": "'$REVISION'",' >> VERSION
+  		echo '    "build": "'$BUILD_NUM'",' >> VERSION
+  		echo '    "date": "'`date +'%d/%m/%Y'`'",' >> VERSION
+  		echo '    "commit": "'$CONFIGURATION_NAME'"' >> VERSION
+  		echo '}' >> VERSION
+  	popd
 	
 	result=`echo $?`
 	echo "### Nosetests exited with code $result"
@@ -27,9 +27,4 @@ do
 		cosmo_unit_tests_fail="$r,$cosmo_unit_tests_fail"			
    		echo "### Unit tests failed for: $r";   			
 	fi
-
-	popd
-	
 done
-
-deactivate
