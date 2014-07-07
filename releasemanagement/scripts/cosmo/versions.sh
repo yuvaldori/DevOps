15afa70ceb0d085c58d5ad5660ffcbfb5f2fcd74
opencm <opencm@gigaspaces.com>
2014-07-07 22:39:36 +0300

    Update versions.sh

--- releasemanagement/scripts/cosmo/versions.sh
+++ releasemanagement/scripts/cosmo/versions.sh
@@ -2,31 +2,52 @@
 
 source retry.sh
 
+if [ "$PACK_CLI" = "yes" ]
+then
+	REPOS_LIST="cloudify-cli/cosmo_cli "
+fi
+if [ "$PACK_CORE" = "yes" ]
+then
+	REPOS_LIST=$REPOS_LIST"cloudify-manager/rest-service/manager_rest "
+fi
+if [ "$PACK_UI" = "yes" ]
+then
+	REPOS_LIST=$REPOS_LIST"cosmo-ui"
+fi
 
-REPOS_LIST="cloudify-manager/rest-service/manager_rest \
-cloudify-cli/cosmo_cli"
+echo "REPOS_LIST
 
 echo "### Repositories list: $REPOS_LIST"
 
 for r in ${REPOS_LIST}
 do
 	echo "### Processing repository: $r"	   	
-	if [[ `git branch | grep temp_version` ]]
+	#if [[ `git branch | grep temp_version` ]]
+	#then
+	    #echo "Branch named temp_version already exists, deleting it"
+	    #git branch -d temp_version
+	#fi
+	#set revision sha
+	if [ "$r" = "cloudify-manager/rest-service/manager_rest" ]
 	then
-	    echo "Branch named temp_version already exists, deleting it"
-	    git branch -d temp_version
+		REVISION=$MANAGER_SHA
+	elif [ "$r"="cloudify-cli/cosmo_cli" ]
+	then
+		REVISION=$CLI_SHA
+	elif [ "$r" = "cosmo-ui" ]
+	then
+		REVISION=$UI_SHA	
 	fi
-
 	pushd $r
 	  	echo '{' > VERSION
-  		echo '    "version": "'$REVISION'",' >> VERSION
+  		echo '    "version": "'$PRODUCT_VERSION'",' >> VERSION
   		echo '    "build": "'$BUILD_NUM'",' >> VERSION
   		echo '    "date": "'`date +'%d/%m/%Y'`'",' >> VERSION
   		echo '    "commit": "'$REVISION'"' >> VERSION
   		echo '}' >> VERSION
   	popd
-  	git checkout -b temp_version
-	git commit -m 'edit VERSION by nightly'
+  	##git checkout -b temp_version
+	##git commit -m 'edit VERSION by nightly'
 	#result=`echo $?`
 	#echo "### Nosetests exited with code $result"
         #if [ $result -ne 0 ]; then
