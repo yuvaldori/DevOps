42719df9cc50ebc49b2917116fd6a78b3aabf0fe
opencm <opencm@gigaspaces.com>
2014-07-10 08:13:18 +0300

    Update create_tags.sh

--- releasemanagement/scripts/cosmo/create_tags.sh
+++ releasemanagement/scripts/cosmo/create_tags.sh
@@ -101,6 +101,7 @@ do
 	 		echo "Branch named $VERSION_BRANCH_NAME already exists, deleting it"
 	 		git branch -D $VERSION_BRANCH_NAME
 	 		exit_on_error
+	 		git push origin --delete $VERSION_BRANCH_NAME
 	 	fi		
 	 		
 		
