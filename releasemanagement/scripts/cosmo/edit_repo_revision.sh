2f791910e0561994767df35060abdad317e28391
opencm <opencm@gigaspaces.com>
2014-07-15 10:10:18 +0300

    Update edit_repo_revision.sh

--- releasemanagement/scripts/cosmo/edit_repo_revision.sh
+++ releasemanagement/scripts/cosmo/edit_repo_revision.sh
@@ -18,14 +18,14 @@ echo "REST_CLIENT_SHA=$REST_CLIENT_SHA"
 echo "CLI_SHA=$CLI_SHA"
 echo "OS_PROVIDER_SHA=$OS_PROVIDER_SHA"
 echo "PLUGIN_COMMON_SHA=$PLUGIN_COMMON_SHA"
-
+echo "PACKMAN_SHA=$PACKMAN_SHA"
 
 #edit the revision number in linux/provision.sh
 fileName="cloudify-cli-packager/vagrant/linux/provision.sh"
-sed -i "s/.*DSL_SHA=.*/DSL_SHA=$DSL_SHA/g" $fileName
-sed -i "s/.*REST_CLIENT_SHA=.*/REST_CLIENT_SHA=$REST_CLIENT_SHA/g" $fileName
-sed -i "s/.*CLI_SHA=.*/CLI_SHA=$CLI_SHA/g" $fileName
-sed -i "s/.*OS_PROVIDER_SHA=.*/OS_PROVIDER_SHA=$OS_PROVIDER_SHA/g" $fileName
+sed -i "s/^DSL_SHA=.*/DSL_SHA=$DSL_SHA/g" $fileName
+sed -i "s/^REST_CLIENT_SHA=.*/REST_CLIENT_SHA=$REST_CLIENT_SHA/g" $fileName
+sed -i "s/^CLI_SHA=.*/CLI_SHA=$CLI_SHA/g" $fileName
+sed -i "s/^OS_PROVIDER_SHA=.*/OS_PROVIDER_SHA=$OS_PROVIDER_SHA/g" $fileName
 
 win_fileName="cloudify-cli-packager/vagrant/windows/provision.bat"
 sed -i "s/.*SET DSL_SHA=.*/SET DSL_SHA=$DSL_SHA/g" $win_fileName
@@ -38,3 +38,8 @@ sed -i "s/.*SET REST_CLIENT_SHA=.*/SET REST_CLIENT_SHA=$REST_CLIENT_SHA/g" $win_
 sed -i "s/.*SET PLUGIN_COMMON_SHA=.*/SET PLUGIN_COMMON_SHA=$PLUGIN_COMMON_SHA/g" $win_agent_fileName
 sed -i "s/.*SET OS_PROVIDER_SHA=.*/SET OS_PROVIDER_SHA=$OS_PROVIDER_SHA/g" $win_agent_fileName
 
+centos_agent_fileName="  cloudify-packager-centos/vagrant/centos-agent/provision.sh"
+sed -i "s/.*SET REST_CLIENT_SHA=.*/SET REST_CLIENT_SHA=$REST_CLIENT_SHA/g" $centos_agent_fileName
+sed -i "s/.*SET PLUGIN_COMMON_SHA=.*/SET PLUGIN_COMMON_SHA=$PLUGIN_COMMON_SHA/g" $centos_agent_fileName
+sed -i "s/.*SET MANAGER_SHA=.*/SET MANAGER_SHA=$MANAGER_SHA/g" $centos_agent_fileName
+sed -i "s/.*SET PACKMAN_SHA=.*/SET PACKMAN_SHA=$PACKMAN_SHA/g" $centos_agent_fileName
