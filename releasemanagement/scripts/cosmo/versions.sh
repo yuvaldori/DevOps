459afc30baba14f5ff9d0a1a58b3a588f318e7f5
opencm <opencm@gigaspaces.com>
2014-07-17 22:25:28 +0300

    Update versions.sh

--- releasemanagement/scripts/cosmo/versions.sh
+++ releasemanagement/scripts/cosmo/versions.sh
@@ -80,6 +80,64 @@ do
 	
 done
 
+if [ "$PACK_CORE" == "yes" ||  "$PACK_CLI" == "yes" ]
+then
+	defaults_config_yaml_file_name="cloudify-config.defaults.yaml"
+	defaults_config_yaml_file="cloudify-openstack-provider/cloudify_openstack/"$defaults_config_yaml_file_name
+	config_yaml_file_name="cloudify-config.yaml"
+	config_yaml_file="cloudify-openstack-provider/cloudify_openstack/"$config_yaml_file_name
+	defaults_cli_config_yaml_file_name="cloudify-config.defaults.yaml"
+	defaults_cli_config_yaml_file="cloudify-cli/cloudify_simple_provider/"$defaults_cli_config_yaml_file_name
+	config_cli_yaml_file_name="cloudify-config.yaml"
+	config_cli_yaml_file="cloudify-cli/cloudify_simple_provider/"$config_cli_yaml_file_name
+	
+
+	#components_package_url=$(grep "cloudify_components_package_url:" cloudify-packager-ubuntu/nightly-aws.links | sed 's/cloudify_components_package_url: //')
+	#core_package_url=$(grep "cloudify_core_package_url:" cloudify-packager-ubuntu/nightly-aws.links | sed 's/cloudify_core_package_url: //')
+	#ui_package_url=$(grep "cloudify_ui_package_url:" cloudify-packager-ubuntu/nightly-aws.links | sed 's/cloudify_ui_package_url: //')
+	#ubuntu_agent_url=$(grep "cloudify_ubuntu_agent_url:" cloudify-packager-ubuntu/nightly-aws.links | sed 's/cloudify_ubuntu_agent_url: //')
+	#windows_agent_url=$(grep "cloudify_windows_agent_url:" cloudify-packager-ubuntu/nightly-aws.links | sed 's/cloudify_windows_agent_url: //')
+	if [ "$RELEASE_BUILD" == "false" ]
+	then
+		components_package_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/nightly/cloudify-components_amd64.deb"
+		core_package_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/nightly/cloudify-core_amd64.deb"
+		ubuntu_agent_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/nightly/cloudify-ubuntu-agent_amd64.deb"
+		centos_agent_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/nightly/cloudify-centos-agent_amd64.deb"
+		windows_agent_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/nightly/cloudify-windows-agent_amd64.deb"
+		ui_package_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/nightly/cloudify-ui_amd64.deb"
+	else
+		ui_package_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/3.0.0/nightly_"$BUILD_NUM"/cloudify-ui_"$PRODUCT_VERSION_FULL"_amd64.deb"
+		core_package_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/3.0.0/nightly_"$BUILD_NUM"/cloudify-core_"$PRODUCT_VERSION_FULL"_amd64.deb"
+		ubuntu_agent_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/3.0.0/nightly_"$BUILD_NUM"/cloudify-ubuntu-agent_"$PRODUCT_VERSION_FULL"_amd64.deb"
+		centos_agent_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/3.0.0/nightly_"$BUILD_NUM"/cloudify-centos-agent_"$PRODUCT_VERSION_FULL"_amd64.deb"
+		windows_agent_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/3.0.0/nightly_"$BUILD_NUM"/cloudify-windows-agent_"$PRODUCT_VERSION_FULL"_amd64.deb"
+		components_package_url="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/3.0.0/nightly_"$BUILD_NUM"/cloudify-components_"$PRODUCT_VERSION_FULL"_amd64.deb"
+	fi
+
+	sed -i "s|{{ components_package_url }}|$(echo ${components_package_url})|g" $defaults_config_yaml_file $config_yaml_file $defaults_cli_config_yaml_file $config_cli_yaml_file
+	sed -i "s|{{ core_package_url }}|$(echo ${core_package_url})|g" $defaults_config_yaml_file $config_yaml_file $defaults_cli_config_yaml_file $config_cli_yaml_file
+	sed -i "s|{{ ui_package_url }}|$(echo ${ui_package_url})|g" $defaults_config_yaml_file $config_yaml_file $defaults_cli_config_yaml_file $config_cli_yaml_file
+	sed -i "s|{{ ubuntu_agent_url }}|$(echo ${ubuntu_agent_url})|g" $defaults_config_yaml_file $config_yaml_file $defaults_cli_config_yaml_file $config_cli_yaml_file
+	sed -i "s|{{ windows_agent_url }}|$(echo ${windows_agent_url})|g" $defaults_config_yaml_file $config_yaml_file $defaults_cli_config_yaml_file $config_cli_yaml_file
+	sed -i "s|{{ centos_agent_url }}|$(echo ${centos_agent_url})|g" $defaults_config_yaml_file $config_yaml_file $defaults_cli_config_yaml_file $config_cli_yaml_file
+	
+	OS_PROVIDER_SHA_file="OS_PROVIDER.SHA"
+	rm -f $OS_PROVIDER_SHA_file
+        pushd cloudify-openstack-provider/cloudify_openstack
+        	git add $defaults_config_yaml_file_name $config_yaml_file_name
+		git commit -m 'replace urls in config yaml files' $defaults_config_yaml_file_name $config_yaml_file_name
+		OS_PROVIDER_SHA=$(git rev-parse HEAD) 
+		echo "$OS_PROVIDER_SHA" > $OS_PROVIDER_SHA_file
+	popd
+	CLI_SHA_file="CLI.SHA"
+	rm -f $CLI_SHA_file
+	pushd cloudify-cli/cloudify_simple_provider
+		git add $defaults_cli_config_yaml_file_name $config_cli_yaml_file_name
+		git commit -m 'replace urls in config yaml files' $defaults_cli_config_yaml_file_name $config_cli_yaml_file_name
+		CLI_SHA=$(git rev-parse HEAD) 
+		echo "$CLI_SHA" > $CLI_SHA_file
+	popd
+fi
 
 echo "### Repositories list: $REPOS_LIST"
 
