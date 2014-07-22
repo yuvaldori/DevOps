34af2d233772d61fd97025f9e73bd03cf8bd0cd8
opencm <opencm@gigaspaces.com>
2014-07-22 14:14:47 +0300

    Create generic_functions.sh

---
+++ releasemanagement/scripts/cosmo/generic_functions.sh
@@ -0,0 +1,43 @@
+function  exit_on_error {
+      status=$?
+      echo "exit code="$status    
+      if [ $status != 0 ] ; then
+         	echo "Failed (exit code $status)" 
+		exit 1
+      fi
+
+}
+
+function  get_product_version_for_npm {
+	case "$MILESTONE" in
+	        ga)
+	            PRODUCT_VERSION=$MAJOR_VERSION"."$MINOR_VERSION"."$SERVICEPACK_VERSION
+	            ;;
+
+	        *)
+	            PRODUCT_VERSION=$MAJOR_VERSION"."$MINOR_VERSION"."$SERVICEPACK_VERSION"-"$MILESTONE
+	 
+	esac
+
+}
+
+function retry 
+{
+   nTrys=0
+   maxTrys=10
+   status=256
+   until [ $status == 0 ] ; do
+      echo "*** Running $1"      
+      $1
+      status=$?
+      nTrys=$(($nTrys + 1))
+      if [ $nTrys -gt $maxTrys ] ; then
+            echo "Number of re-trys exceeded. Exit code: $status"
+            exit $status
+      fi
+      if [ $status != 0 ] ; then
+            echo "Failed (exit code $status)... retry $nTrys"
+            sleep 15
+      fi
+   done
+}
