e1ccf40ce2d7ecdcd235b112e28879b7209b0b4e
opencm <opencm@gigaspaces.com>
2014-07-30 17:40:04 +0300

    Update copy_release_packages_to_website.sh
    add rpm and license package

--- releasemanagement/scripts/upload_release/copy_release_packages_to_website.sh
+++ releasemanagement/scripts/upload_release/copy_release_packages_to_website.sh
@@ -5,7 +5,11 @@ source params.sh
 ssh  -i ~/.ssh/website  tempfiles@www.gigaspaces.com "mkdir -p ~/download_files/${MAJOR_VERSION}/${VERSION}/"
 
 #for file in `find ${BUILD_DIR} -name "*.zip" -o -name "*.tar.gz" -o -name "*.msi"  | grep -v license | grep -v testsuite | grep -v xap-premium.1.5`; do
-for file in `find ${BUILD_DIR} -name "*.zip" -o -name "*.tar.gz" -o -name "*.msi"  | grep -v license | grep -v testsuite`; do
+#for file in `find ${BUILD_DIR} -name "*.zip" -o -name "*.tar.gz" -o -name "*.msi"  | grep -v license | grep -v testsuite`; do
+
+for file in `find ${BUILD_DIR} -name "*.zip" -o -name "*.tar.gz" -o -name "*.msi" -o -name "*.rpm" | grep -v license | grep -v testsuite`; do
+
   echo Uploading $file to ~/download_files/${MAJOR_VERSION}/${VERSION}/
   scp -i ~/.ssh/website $file tempfiles@www.gigaspaces.com:~/download_files/${MAJOR_VERSION}/${VERSION}/
+
 done
