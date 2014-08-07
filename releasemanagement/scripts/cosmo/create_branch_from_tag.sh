dfbf00fc81b7d2e12fc801cc1f86633050ce6ebc
opencm <opencm@gigaspaces.com>
2014-08-07 18:56:05 +0300

    Update create_branch_from_tag.sh

--- releasemanagement/scripts/cosmo/create_branch_from_tag.sh
+++ releasemanagement/scripts/cosmo/create_branch_from_tag.sh
@@ -41,7 +41,8 @@ do
 		git pull
 	else
 		if [ "${project}" == "cosmo-ui" ]; then 
-		  git clone "https://opencm:${GIT_PWD}@github.com/CloudifySource/${project}.git"
+		  #git clone "https://opencm:${GIT_PWD}@github.com/CloudifySource/${project}.git"
+		  git clone "https://github.com/CloudifySource/${project}.git"
 		  exit_on_error
 		else
 		  git clone "https://opencm:${GIT_PWD}@github.com/cloudify-cosmo/${project}.git"
