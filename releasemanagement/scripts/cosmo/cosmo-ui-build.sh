2d2dbbc8e612b9fee17de38e1c2a8af7796bee83
limor-gs <limor@gigaspaces.com>
2014-09-08 11:58:04 +0300

    Update cosmo-ui-build.sh

--- releasemanagement/scripts/cosmo/cosmo-ui-build.sh
+++ releasemanagement/scripts/cosmo/cosmo-ui-build.sh
@@ -5,6 +5,8 @@ source ../generic_functions.sh
 [ -d dist ] && sudo rm -rf dist
 [ -d node_modules ] && sudo rm -rf node_modules
 
+fromdos ~/.npm/connect/2.7.11/package/package.json
+
 sudo npm cache clean
 sudo bower cache clean
 
@@ -18,18 +20,24 @@ fi
 
 run_command "grunt build"
 
+if [ $(basename `pwd`) = "grafana" ]
+then
+	cp -fp package.json dist
+fi
 
 pushd dist
-	echo '{' > views/versionDetails.html
-	echo '    "revision": "'$REVISION'",' >> views/versionDetails.html
-	echo '    "buildVersion": "'$BUILD_NUM'",' >> views/versionDetails.html
-	echo '    "buildId": "'$BUILD_ID'",' >> views/versionDetails.html
-	echo '    "configurationName": "'$CONFIGURATION_NAME'",' >> views/versionDetails.html
-	echo '    "timestamp": "'`date +%T`'",' >> views/versionDetails.html
-	echo '    "date": "'`date +'%d/%m/%Y'`'"' >> views/versionDetails.html
-	echo '}' >> views/versionDetails.html
+	if [ $(basename `pwd`) = "cosmo-ui" ]
+	then
+		echo '{' > views/versionDetails.html
+		echo '    "revision": "'$REVISION'",' >> views/versionDetails.html
+		echo '    "buildVersion": "'$BUILD_NUM'",' >> views/versionDetails.html
+		echo '    "buildId": "'$BUILD_ID'",' >> views/versionDetails.html
+		echo '    "configurationName": "'$CONFIGURATION_NAME'",' >> views/versionDetails.html
+		echo '    "timestamp": "'`date +%T`'",' >> views/versionDetails.html
+		echo '    "date": "'`date +'%d/%m/%Y'`'"' >> views/versionDetails.html
+		echo '}' >> views/versionDetails.html
 
-
+	fi
 	run_command "npm install --production"
 	run_command "npm pack"
 #popd
