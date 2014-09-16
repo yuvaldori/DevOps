087b8be07df18d6e3cc787ab9c0b69dc44b7841b
limor-gs <limor@gigaspaces.com>
2014-09-16 10:54:12 +0300

    Update edit_repo_revision.sh

--- releasemanagement/scripts/cosmo/edit_repo_revision.sh
+++ releasemanagement/scripts/cosmo/edit_repo_revision.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-#DSL_SHA=d2bedb39b62bddc3888c09e21163ab987f3d4883
-#REST_CLIENT_SHA=b84591d8fb024f9bbf4679f361d15ec18fc1cae1
-#CLI_SHA=1fee425e6a2f21a231f023fae4d7885ad07a0e4e
-#OS_PROVIDER_SHA=2fa30b0b6b9c2e20068ed3d68e160390f309fcee
-#PLUGIN_COMMON_SHA=2fa30b0b6b9c2e20068ed3d68e160390f30999999
 
-
-DSL_SHA=$(echo $DSL_SHA)
-REST_CLIENT_SHA=$(echo $REST_CLIENT_SHA)
-CLI_SHA=$(echo $CLI_SHA)
-OS_PROVIDER_SHA=$(echo $OS_PROVIDER_SHA)
-PLUGIN_COMMON_SHA=$(echo $PLUGIN_COMMON_SHA)
+#DSL_SHA=$(echo $DSL_SHA)
+#REST_CLIENT_SHA=$(echo $REST_CLIENT_SHA)
+#CLI_SHA=$(echo $CLI_SHA)
+#OS_PROVIDER_SHA=$(echo $OS_PROVIDER_SHA)
+#PLUGIN_COMMON_SHA=$(echo $PLUGIN_COMMON_SHA)
 
 echo "DSL_SHA=$DSL_SHA"
 echo "REST_CLIENT_SHA=$REST_CLIENT_SHA"
@@ -22,10 +16,10 @@ echo "PACKMAN_SHA=$PACKMAN_SHA"
 
 #edit the revision number in linux/provision.sh
 fileName="cloudify-cli-packager/vagrant/linux/provision.sh"
-sed -i "s/^DSL_SHA=.*/DSL_SHA=$DSL_SHA/g" $fileName
-sed -i "s/^REST_CLIENT_SHA=.*/REST_CLIENT_SHA=$REST_CLIENT_SHA/g" $fileName
-sed -i "s/^CLI_SHA=.*/CLI_SHA=$CLI_SHA/g" $fileName
-sed -i "s/^OS_PROVIDER_SHA=.*/OS_PROVIDER_SHA=$OS_PROVIDER_SHA/g" $fileName
+sed -i "s/.*DSL_SHA=.*/DSL_SHA=$DSL_SHA/g" $fileName
+sed -i "s/.*REST_CLIENT_SHA=.*/REST_CLIENT_SHA=$REST_CLIENT_SHA/g" $fileName
+sed -i "s/.*CLI_SHA=.*/CLI_SHA=$CLI_SHA/g" $fileName
+sed -i "s/.*OS_PROVIDER_SHA=.*/OS_PROVIDER_SHA=$OS_PROVIDER_SHA/g" $fileName
 
 win_fileName="cloudify-cli-packager/vagrant/windows/provision.bat"
 sed -i "s/.*SET DSL_SHA=.*/SET DSL_SHA=$DSL_SHA/g" $win_fileName
