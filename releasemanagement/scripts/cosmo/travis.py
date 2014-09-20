e1f550f5d02346a82c6fffa27f3a2b0b8c56bbf7
limor-gs <limor@gigaspaces.com>
2014-09-20 23:58:26 +0300

    Update travis.py

--- releasemanagement/scripts/cosmo/travis.py
+++ releasemanagement/scripts/cosmo/travis.py
@@ -21,48 +21,50 @@ branch_name=os.environ["BRANCH_NAME"]
 release_build=os.environ["RELEASE_BUILD"]
 core_branch_name=os.environ["RELEASE_CORE_BRANCH_NAME"]
 plugins_branch_name=os.environ["RELEASE_PLUGINS_BRANCH_NAME"]
+travis_repos=['cloudify-rest-client','cloudify-dsl-parser','cloudify-plugins-common','cloudify-cli','cloudify-manager','cloudify-bash-plugin','cloudify-openstack-provider','cloudify-chef-plugin','cloudify-fabric-plugin','cloudify-openstack-plugin','cloudify-script-plugin']
 
 
 d = json.loads(tests_repos_sha_list)
 
 for repo,sha in d.items():
-	print repo
-	print sha
-	if release_build == "true":
-		get_name=subprocess.Popen(['bash', '-c', '. generic_functions.sh ; get_version_name {0} {1} {2}'.format(repo, a, b)],stdout = subprocess.PIPE).communicate()[0]
-		branch_name=get_name.rstrip()+"_build"
-	print "branch_name="+branch_name
+	if repo in travis_repos:
+		print repo
+		print sha
+		if release_build == "true":
+			get_name=subprocess.Popen(['bash', '-c', '. generic_functions.sh ; get_version_name {0} {1} {2}'.format(repo, a, b)],stdout = subprocess.PIPE).communicate()[0]
+			branch_name=get_name.rstrip()+"_build"
+		print "branch_name="+branch_name
 	
-	if repo == 'cosmo-ui':
-		parent_repo='CloudifySource/'
-	else:
-		parent_repo='cloudify-cosmo/'
-	try:
+		if repo == 'cosmo-ui':
+			parent_repo='CloudifySource/'
+		else:
+			parent_repo='cloudify-cosmo/'
+		try:
 
-		jobs_state = yoci.travis.functional_api.get_jobs_status(sha,
-		parent_repo+repo,
-		branch_name=branch_name,
-		timeout_min=1)
-		for key,value in jobs_state.items():
-			#print(key, ":", value)
-			if value=='passed':
-				print key + ' success'
-			else:
-				if repo == "cloudify-manager" and "run-integration-tests" in key:
-					print 'integration tests failed'
-					f1 = open(itests_fail_file, 'w')
-					f1.write("failed")
-					f1.close()
-				elif repo not in fail_repos:
-					fail_repos=fail_repos+','+repo
-					print key + ' failure'
+			jobs_state = yoci.travis.functional_api.get_jobs_status(sha,
+			parent_repo+repo,
+			branch_name=branch_name,
+			timeout_min=1)
+			for key,value in jobs_state.items():
+				#print(key, ":", value)
+				if value=='passed':
+					print key + ' success'
 				else:
-					print key + ' failure'
+					if repo == "cloudify-manager" and "run-integration-tests" in key:
+						print 'integration tests failed'
+						f1 = open(itests_fail_file, 'w')
+						f1.write("failed")
+						f1.close()
+					elif repo not in fail_repos:
+						fail_repos=fail_repos+','+repo
+						print key + ' failure'
+					else:
+						print key + ' failure'
 				
 		
-	except RuntimeError:
-		print 'Exception'
-		fail_repos=fail_repos+','+repo		
+		except RuntimeError:
+			print 'Exception'
+			fail_repos=fail_repos+','+repo		
 
 if fail_repos:
 	print 'fail_repos='+fail_repos
