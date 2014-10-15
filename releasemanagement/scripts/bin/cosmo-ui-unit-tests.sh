70f24a049b0a9a3ed12c5e3e19d75ffd71ebde1c
limor-gs <limor@gigaspaces.com>
2014-10-15 17:26:03 +0300

    Create cosmo-ui-unit-tests.sh

---
+++ releasemanagement/scripts/bin/cosmo-ui-unit-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+source generic_functions.sh
+
+branch_names=()
+git fetch -v --dry-run >fetch.output 2>&1
+IFS=$'\n'; list=($(cat fetch.output | grep -v 'up to date' | grep -v 'From https'))
+
+for line in "${list[@]}"
+do
+        echo $line
+        if [[ $line =~ '[new branch]' ]]
+        then
+                #echo "[new branch]"
+                #echo $line | awk '{ print $4 }'
+                branch_names+=($(echo $line | awk '{ print $4 }'))
+        else
+                #echo "stam"
+                #echo $line | awk '{ print $2 }'
+                branch_names+=($(echo $line | awk '{ print $2 }'))
+        fi
+done
+
+for branch in  "${branch_names[@]}"
+do
+        git checkout $branch
+        retry "bower install -f"
+        retry "bower update -f"
+        run_command "grunt test"
+done
