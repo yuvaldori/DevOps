6163686f1f2513b4d6e31bf29c3b1765c29076c7
limor-gs <limor@gigaspaces.com>
2014-10-19 12:11:56 +0300

    Update cosmo-ui-unit-tests.sh

--- releasemanagement/scripts/cosmo/cosmo-ui-unit-tests.sh
+++ releasemanagement/scripts/cosmo/cosmo-ui-unit-tests.sh
@@ -9,29 +9,35 @@ source generic_functions.sh
 branch_names=()
 git fetch -v --dry-run >fetch.output 2>&1
 IFS=$'\n'; list=($(cat fetch.output | grep -v 'up to date' | grep -v 'From https'))
-for line in "${list[@]}"
-do
-  echo $line
-  if [[ $line =~ '[new branch]' ]]
-  then
-    #echo "[new branch]"
-    #echo $line | awk '{ print $4 }'
-    branch_names+=($(echo $line | awk '{ print $4 }'))
-  else
-    #echo "stam"
-    #echo $line | awk '{ print $2 }'
-    branch_names+=($(echo $line | awk '{ print $2 }'))
-  fi
-done
+if [[ $list ]]
+then
+  for line in "${list[@]}"
+  do
+    echo $line
+    if [[ $line =~ '[new branch]' ]]
+    then
+      #echo "[new branch]"
+      #echo $line | awk '{ print $4 }'
+      branch_names+=($(echo $line | awk '{ print $4 }'))
+    else
+      #echo "stam"
+      #echo $line | awk '{ print $2 }'
+      branch_names+=($(echo $line | awk '{ print $2 }'))
+    fi
+  done
 
 
-for branch in "${branch_names[@]}"
-do
-  git checkout $branch
-  sudo rm -rf node_modules/ 
-  rm -rf app/bower_components/
-  retry "sudo npm install"
-  retry "bower install -f"
-  retry "bower update -f"
-  run_command "grunt test"
-done
+  for branch in "${branch_names[@]}"
+  do
+    git checkout $branch
+    sudo rm -rf node_modules/ 
+    rm -rf app/bower_components/
+    retry "sudo npm install"
+    retry "bower install -f"
+    retry "bower update -f"
+    run_command "grunt test"
+  done
+else
+  echo "Everything up-to-date"
+fi
+  
