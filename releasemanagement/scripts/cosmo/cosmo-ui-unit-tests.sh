f78f2629591d5e2b4aa19af48d4586ed92a2c074
limor-gs <limor@gigaspaces.com>
2014-10-19 10:22:05 +0300

    Update cosmo-ui-unit-tests.sh

--- releasemanagement/scripts/cosmo/cosmo-ui-unit-tests.sh
+++ releasemanagement/scripts/cosmo/cosmo-ui-unit-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #sudo add-apt-repository ppa:chris-lea/node.js 
 #sudo apt-get update
@@ -11,22 +11,23 @@ git fetch -v --dry-run >fetch.output 2>&1
 IFS=$'\n'; list=($(cat fetch.output | grep -v 'up to date' | grep -v 'From https'))
 for line in "${list[@]}"
 do
-echo $line
-if [[ $line =~ '[new branch]' ]]
-then
-#echo "[new branch]"
-#echo $line | awk '{ print $4 }'
-branch_names+=($(echo $line | awk '{ print $4 }'))
-else
-#echo "stam"
-#echo $line | awk '{ print $2 }'
-branch_names+=($(echo $line | awk '{ print $2 }'))
-fi
+  echo $line
+  if [[ $line =~ '[new branch]' ]]
+  then
+    #echo "[new branch]"
+    #echo $line | awk '{ print $4 }'
+    branch_names+=($(echo $line | awk '{ print $4 }'))
+  else
+    #echo "stam"
+    #echo $line | awk '{ print $2 }'
+    branch_names+=($(echo $line | awk '{ print $2 }'))
+  fi
 done
+
 for branch in "${branch_names[@]}"
 do
-git checkout $branch
-retry "bower install -f"
-retry "bower update -f"
-run_command "grunt test"
+  git checkout $branch
+  retry "bower install -f"
+  retry "bower update -f"
+  run_command "grunt test"
 done
