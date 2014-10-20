fbe13d7d7c7a0ba5a89ccb436e0bb432b2144f9f
limor-gs <limor@gigaspaces.com>
2014-10-20 10:32:12 +0300

    Update cosmo-ui-unit-tests.sh

--- releasemanagement/scripts/cosmo/cosmo-ui-unit-tests.sh
+++ releasemanagement/scripts/cosmo/cosmo-ui-unit-tests.sh
@@ -7,27 +7,6 @@
 
 source generic_functions.sh
 
-function retry
-{
-  nTrys=0
-  maxTrys=10
-  status=256
-  until [ $status == 0 ] ; do
-  echo "*** Running $1"
-  $1
-  status=$?
-  nTrys=$(($nTrys + 1))
-  if [ $nTrys -gt $maxTrys ] ; then
-  echo "Number of re-trys exceeded. Exit code: $status"
-  exit $status
-  fi
-  if [ $status != 0 ] ; then
-  echo "Failed (exit code $status)... retry $nTrys"
-  sleep 15
-  fi
-  done
-}
-
 branch_names=()
 git fetch -v --dry-run >fetch.output 2>&1
 IFS=$'\n'; list=($(cat fetch.output | grep -v 'up to date' | grep -v 'From https')) ; echo "list=${list[@]}"
@@ -38,12 +17,8 @@ then
     echo line=$line
     if [[ $line =~ '[new branch]' ]]
     then
-      #echo "[new branch]"
-      #echo $line | awk '{ print $4 }'
       branch_names+=($(echo $line | awk '{ print $4 }'))
     else
-      #echo "stam"
-      #echo $line | awk '{ print $2 }'
       branch_names+=($(echo $line | awk '{ print $2 }'))
     fi
   done
