1a54bc364f54e1ed4cf139c5332b64a34c6040e2
limor-gs <limor@gigaspaces.com>
2014-10-26 16:51:44 +0200

    Create core-ci.sh

---
+++ releasemanagement/scripts/cosmo/core-ci.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -x
+
+source generic_functions.sh
+branch_names=()
+git checkout master
+git fetch -v --dry-run > fetch.output 2>&1
+IFS=$'\n'; list=($(cat fetch.output | grep -v 'up to date' | grep -v 'POST git-upload-pack' | grep -v 'From https')) ; echo "***list=${list[@]}"
+unset IFS
+git checkout master
+git pull
+
+[ -f send.email ] && rm -f send.email
+
+if [[ $list ]]
+then
+echo "yes" > send.email
+sudo npm cache clean
+sudo bower cache clean
+for line in "${list[@]}"
+  do
+    echo line=$line
+  if [[ $line =~ '[new branch]' ]]
+  then
+    branch_names+=($(echo $line | awk '{ print $4 }'))
+  else
+    commit=($(echo $line | awk '{ print $1 }')) 
+    echo '***commit=$commit'
+    echo "files=$(git show --name-only $commit | grep 'core/\|openspaces/')"
+    if [[ $(git show --name-only $commit | grep 'core/\|openspaces/') ]]
+    then
+      branch_names+=($(echo $line | awk '{ print $2 }'))
+    fi
+  fi
+done
+IFS=$'\n'; echo "***branch_names=${branch_names[@]}"
+unset IFS
+
+#for branch in "${branch_names[@]}"
+#do
+#  git checkout $branch
+#done
+#else
+#  echo "***Everything up-to-date***"
+#fi
