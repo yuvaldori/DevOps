7d4d3e5e165ebae7d8de5b3ccd8a984158f6a72d
limor-gs <limor@gigaspaces.com>
2014-11-04 13:41:01 +0200

    Update core-ci.sh

--- releasemanagement/scripts/cosmo/core-ci.sh
+++ releasemanagement/scripts/cosmo/core-ci.sh
@@ -28,7 +28,7 @@ then
         commit=$(echo "$line" | awk '{ print $1 }')
         echo "***commit=$commit"
         echo "***files=$(git show --name-only $commit)"
-        if [[ $(git show --name-only $commit | grep 'core/\|openspaces/') ]]
+        if [[ $(git show --name-only $commit | grep 'core/\|openspaces/\|tests/') ]]
         then
           branch_names+=($(echo "$line" | awk '{ print $2 }'))
           echo "yes" > ../send.email
