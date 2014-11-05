1eea3364060c2843b4d8cc0c41db69cfc5172a05
limor-gs <limor@gigaspaces.com>
2014-11-05 10:56:34 +0200

    Update core-ci.sh

--- releasemanagement/scripts/cosmo/core-ci.sh
+++ releasemanagement/scripts/cosmo/core-ci.sh
@@ -6,8 +6,8 @@ source ../params.sh
 branch_names=()
 git checkout master
 
-git fetch -v --dry-run > fetch.output 2>&1
-IFS=$'\n'; list=($(cat fetch.output | grep -v 'up to date' | grep -v 'POST git-upload-pack' | grep -v 'From https' | grep -v 'error:')) ; echo "***list=${list[@]}"
+git fetch -v --dry-run > ../fetch.output 2>&1
+IFS=$'\n'; list=($(cat ../fetch.output | grep -v 'up to date' | grep -v 'POST git-upload-pack' | grep -v 'From https' | grep -v 'error:')) ; echo "***list=${list[@]}"
 unset IFS
 git checkout master
 git pull
