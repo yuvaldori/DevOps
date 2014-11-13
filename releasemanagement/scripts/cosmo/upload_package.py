514cf7f7a58f9c65e81d6553595f2ff593568f86
limor-gs <limor@gigaspaces.com>
2014-11-13 18:47:45 +0200

    Update upload_package.py

--- releasemanagement/scripts/cosmo/upload_package.py
+++ releasemanagement/scripts/cosmo/upload_package.py
@@ -117,14 +117,14 @@ if PACK_COMPONENTS == "yes":
 
 if PACK_AGENT == "yes":
 	centos_final_agent_conf = packages.PACKAGES['cloudify-centos-final-agent']
-	centos_agent_final_name = centos_final_agent_conf.name
+	centos_agent_final_name = centos_final_agent_conf'[name']
 	#centos_agent_name="cloudify-centos-agent"
 	#ubuntu_agent_name="cloudify-ubuntu-agent"
 
 	ubuntu_agent_trusty_conf = packages.PACKAGES['cloudify-ubuntu-trusty-agent']
 	ubuntu_agent_precise_conf = packages.PACKAGES['cloudify-ubuntu-precise-agent']
-	ubuntu_agent_trusty_name = ubuntu_agent_trusty_conf.name
-	ubuntu_agent_precise_name = ubuntu_agent_trusty_conf.name
+	ubuntu_agent_trusty_name = ubuntu_agent_trusty_conf['name']
+	ubuntu_agent_precise_name = ubuntu_agent_trusty_conf['name']
 	
 	
 	#print "copy 3rd parties deb from /packages folder"
