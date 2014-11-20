7810d9f6a4af3eb4a92c830ede1360bf0a241594
limor-gs <limor@gigaspaces.com>
2014-11-20 12:37:17 +0200

    Update travis.py

--- releasemanagement/scripts/cosmo/travis.py
+++ releasemanagement/scripts/cosmo/travis.py
@@ -36,7 +36,9 @@ for repo,sha in d.items():
 		print repo
 		print sha
 		get_name=subprocess.Popen(['bash', '-c', '. generic_functions.sh ; get_version_name {0} {1} {2}'.format(repo, core_branch_name, plugins_branch_name)],stdout = subprocess.PIPE).communicate()[0]
-		branch_name=get_name.rstrip()+"-build"
+		#branch_name=get_name.rstrip()+"-build"
+		#make travis api waiting for tag tests
+		branch_name=get_name.rstrip()
 		print "branch_name="+branch_name
 	
 		if repo == 'cosmo-ui':
