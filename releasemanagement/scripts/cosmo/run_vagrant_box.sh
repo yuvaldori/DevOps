8aad29ba1783fe9247f09adbbc0e02d2ab04027c
limor-gs <limor@gigaspaces.com>
2014-12-24 08:39:33 +0200

    Create run_vagrant_box.sh

---
+++ releasemanagement/scripts/cosmo/run_vagrant_box.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ../../ec2dev_credentials.sh
+
+python nightly-builder.py
+
+pushd /cloudify
+  wget http://cloudify-nightly-vagrant.s3-website-eu-west-1.amazonaws.com/Vagrantfile
+  vagrant_box_name=$(basename $(grep -o box_url.* Vagrantfile | grep -o \".*\" | sed -e "s/\"//g"))
+  wget http://cloudify-nightly-vagrant.s3-website-eu-west-1.amazonaws.com/$vagrant_box_name
+popd
+
