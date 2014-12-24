9aafddffbe1673490d180c3f71520fd7fb6dfcdc
limor-gs <limor@gigaspaces.com>
2014-12-24 09:27:18 +0200

    Update run_vagrant_box.sh

--- releasemanagement/scripts/cosmo/run_vagrant_box.sh
+++ releasemanagement/scripts/cosmo/run_vagrant_box.sh
@@ -5,9 +5,10 @@ source /etc/environment
 
 #python nightly-builder.py
 
+sudo chown tgrid -R /cloudify
 pushd /cloudify
-  wget http://cloudify-nightly-vagrant.s3-website-eu-west-1.amazonaws.com/Vagrantfile
+  wget http://cloudify-nightly-vagrant.s3.amazonaws.com/Vagrantfile
   vagrant_box_name=$(basename $(grep -o box_url.* Vagrantfile | grep -o \".*\" | sed -e "s/\"//g"))
-  wget http://cloudify-nightly-vagrant.s3-website-eu-west-1.amazonaws.com/$vagrant_box_name
+  wget http://cloudify-nightly-vagrant.s3.amazonaws.com/$vagrant_box_name
 popd
 
