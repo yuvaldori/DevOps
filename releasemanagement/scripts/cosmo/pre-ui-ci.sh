410e50222bf401a6f3e82f5308a86207d13d19fc
limor-gs <limor@gigaspaces.com>
2014-12-31 11:05:00 +0200

    Update pre-ui-ci.sh

--- releasemanagement/scripts/cosmo/pre-ui-ci.sh
+++ releasemanagement/scripts/cosmo/pre-ui-ci.sh
@@ -2,4 +2,4 @@
 
 source generic_functions.sh
 source params.sh
-pre_git_pull cosmo-ui https://opencm:${GIT_PWD}@github.com/cloudify-cosmo/cloudify-ui.git
+pre_git_pull cloudify-ui https://opencm:${GIT_PWD}@github.com/cloudify-cosmo/cloudify-ui.git
