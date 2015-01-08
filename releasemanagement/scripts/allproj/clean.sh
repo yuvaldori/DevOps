6a898ae01ac26172215bd1842046f1e786c92ca8
limor-gs <limor@gigaspaces.com>
2015-01-08 15:13:17 +0200

    Update clean.sh

--- releasemanagement/scripts/allproj/clean.sh
+++ releasemanagement/scripts/allproj/clean.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
   
-  for dir in `pwd`/*/
-  do
-          dir=${dir%*/}
-          repo=${dir##*/}
+for dir in `pwd`/*/
+do
+    dir=${dir%*/}
+    repo=${dir##*/}
   		
-  	echo "### Processing repository: $repo"
-  	pushd $repo
-  	  [ -d ".git" ] && git clean -df
+    echo "### Processing repository: $repo"
+    pushd $repo
+      [ -d ".git" ] && git clean -df
     popd
-  done
+done
