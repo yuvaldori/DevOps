2f8e886595463cc351aabb764aaaf0bdd5716476
limor-gs <limor@gigaspaces.com>
2015-01-29 12:19:17 +0200

    Update run_vagrant_box.sh

--- releasemanagement/scripts/cosmo/run_vagrant_box.sh
+++ releasemanagement/scripts/cosmo/run_vagrant_box.sh
@@ -3,7 +3,18 @@
 source ../../credentials.sh
 source /etc/environment
 
+function  exit_on_error {
+      status=$?
+      echo "exit code="$status    
+      if [ $status != 0 ] ; then
+         	echo "Failed (exit code $status)"
+		      exit 1
+      fi
+
+}
+
 python nightly-builder.py
+exit_on_error
 
 sudo chown tgrid -R /cloudify
 pushd /cloudify
