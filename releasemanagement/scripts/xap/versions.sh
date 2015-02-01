dd7ce81a321a7eb41edaa3eb3ea366b7e35bf1f2
limor-gs <limor@gigaspaces.com>
2015-02-01 10:36:06 +0200

    Update versions.sh

--- releasemanagement/scripts/xap/versions.sh
+++ releasemanagement/scripts/xap/versions.sh
@@ -19,55 +19,58 @@ for dir in `pwd`/*/
 do
         dir=${dir%*/}
         repo=${dir##*/}
-		
-	echo "### Processing repository: $repo"
-	pushd $repo
-		git pull origin master
-		
-		if [ -n "$BRANCH_NAME_FOR_TEST" ] && [[ `git branch -r | grep origin/$BRANCH_NAME_FOR_TEST` ]]
-		then
-			if [[ `git branch | grep $BRANCH_NAME_FOR_TEST` ]] 
+        
+        if [ "$repo" != "examples" ]
+	then
+		echo "### Processing repository: $repo"
+		pushd $repo
+			git pull origin master
+			
+			if [ -n "$BRANCH_NAME_FOR_TEST" ] && [[ `git branch -r | grep origin/$BRANCH_NAME_FOR_TEST` ]]
 			then
-				git checkout $BRANCH_NAME_FOR_TEST
-			else			
-				git checkout -b $BRANCH_NAME_FOR_TEST origin/$BRANCH_NAME_FOR_TEST
+				if [[ `git branch | grep $BRANCH_NAME_FOR_TEST` ]] 
+				then
+					git checkout $BRANCH_NAME_FOR_TEST
+				else			
+					git checkout -b $BRANCH_NAME_FOR_TEST origin/$BRANCH_NAME_FOR_TEST
+				fi
+				exit_on_error
+				git reset --hard origin/$BRANCH_NAME_FOR_TEST
+				exit_on_error
+			else
+				git checkout $BRANCH_NAME
+				git reset --hard origin/$BRANCH_NAME
 			fi
-			exit_on_error
-			git reset --hard origin/$BRANCH_NAME_FOR_TEST
-			exit_on_error
-		else
-			git checkout $BRANCH_NAME
-			git reset --hard origin/$BRANCH_NAME
-		fi
-		
-		if [ "$RELEASE_BUILD" == "true" ]
-	 	then
- 			if [[ `git branch | grep $VERSION_BRANCH_NAME` ]]
- 			then
- 				git checkout $VERSION_BRANCH_NAME
- 				exit_on_error
- 				git reset --hard origin/$VERSION_BRANCH_NAME
- 				exit_on_error
- 			elif [[ `git branch -r | grep origin/$VERSION_BRANCH_NAME` ]]
- 			then
- 				git checkout -b $VERSION_BRANCH_NAME origin/$VERSION_BRANCH_NAME
- 				exit_on_error
- 				git reset --hard origin/$VERSION_BRANCH_NAME
- 				exit_on_error
- 			else
- 				git checkout -b $VERSION_BRANCH_NAME
- 				exit_on_error
- 				git push origin $VERSION_BRANCH_NAME
-		 		exit_on_error
- 			fi
-		fi
-		sha=$(git rev-parse HEAD)
-	 	if [[ -z "$repo_names_sha" ]];then
-	 		repo_names_sha='[ "'$repo'":"'$sha'"'	
-	 	else
-	 		repo_names_sha=$repo_names_sha',"'$repo'":"'$sha'"'
-	 	fi
-	popd
+			
+			if [ "$RELEASE_BUILD" == "true" ]
+		 	then
+	 			if [[ `git branch | grep $VERSION_BRANCH_NAME` ]]
+	 			then
+	 				git checkout $VERSION_BRANCH_NAME
+	 				exit_on_error
+	 				git reset --hard origin/$VERSION_BRANCH_NAME
+	 				exit_on_error
+	 			elif [[ `git branch -r | grep origin/$VERSION_BRANCH_NAME` ]]
+	 			then
+	 				git checkout -b $VERSION_BRANCH_NAME origin/$VERSION_BRANCH_NAME
+	 				exit_on_error
+	 				git reset --hard origin/$VERSION_BRANCH_NAME
+	 				exit_on_error
+	 			else
+	 				git checkout -b $VERSION_BRANCH_NAME
+	 				exit_on_error
+	 				git push origin $VERSION_BRANCH_NAME
+			 		exit_on_error
+	 			fi
+			fi
+			sha=$(git rev-parse HEAD)
+		 	if [[ -z "$repo_names_sha" ]];then
+		 		repo_names_sha='[ "'$repo'":"'$sha'"'	
+		 	else
+		 		repo_names_sha=$repo_names_sha',"'$repo'":"'$sha'"'
+		 	fi
+		popd
+	fi
 	
 done
 repo_names_sha=$repo_names_sha' ]'
