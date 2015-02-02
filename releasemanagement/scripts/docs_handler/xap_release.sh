66cce437404d0c69382ed52c85dda9a3254f5e4a
limor-gs <limor@gigaspaces.com>
2015-02-02 19:46:05 +0200

    Add cassandra docs

--- releasemanagement/scripts/docs_handler/xap_release.sh
+++ releasemanagement/scripts/docs_handler/xap_release.sh
@@ -3,12 +3,6 @@
 . ./setenv.sh
 source params.sh
 
-#MAJOR_VERSION=9
-#API_DOC_VERSION=${MAJOR_VERSION}.6
-#XAP_RELEASE_VERSION=${API_DOC_VERSION}.2
-#MILESTONE=ga
-#BUILD_NUM=9900
-
 XAP_DIR=gigaspaces-xap-premium-${XAP_RELEASE_VERSION}-${MILESTONE}
 XAP_ZIP_FILENAME=${XAP_DIR}-b${BUILD_NUM}.zip
 DOTNET_HELP_FILE=gigaspaces-xap.net-${XAP_RELEASE_VERSION}-${MILESTONE}-b${BUILD_NUM}-doc.zip
@@ -19,7 +13,7 @@ DOTNETDOC_LINK=dotnetdocs${API_DOC_VERSION}
 CPPDOC_LINK=cppdocs${API_DOC_VERSION}
 SCALADOC_LINK=scaladocs${API_DOC_VERSION}
 MONGOEDSDOC_LINK=mongoeds-docs${API_DOC_VERSION}
-
+CASSANDRA_LINK=cassandra-docs${API_DOC_VERSION}
 
 # DOCS DIRS
 JAVA_DOCS_DIR=${XAP_DIR}/docs/xap-javadoc 
@@ -27,6 +21,7 @@ DOTNET_DOCS_DIR=${XAP_DIR}/dotnet/docs
 CPP_DOCS_DIR=${XAP_DIR}/cpp/docs/html
 SCALA_DOCS_DIR=${XAP_DIR}/docs/scaladocs
 MONGOEDS_DOCS_DIR=${XAP_DIR}/docs/mongoeds-docs
+CASSANDRA_DOCS_DIR=${XAP_DIR}/docs/cassandra-docs
 
 cp ${XAP_RELEASE_DOWNLOAD_DIR}/${MAJOR_NEW}/${XAP_RELEASE_VERSION}/${XAP_ZIP_FILENAME} ${DOC_DIR}
 
@@ -55,7 +50,7 @@ pushd ${DOC_DIR}
 
 	cp -R ${DOC_DIR}/gigaspaces-xap-premium-9.6.2-ga/cpp  ${XAP_DIR} 
 	pushd ${XAP_DIR}/cpp
-		find .  -name '*.html' -exec sed -i "s/XAP 9.6/XAP ${MAJOR_NEW}.${MINOR_NEW}/g" '{}' \; 
+		find .  -name '*.html' -exec sed -i "s/XAP 9.7/XAP ${MAJOR_NEW}.${MINOR_NEW}/g" '{}' \; 
 	popd
 	
 	###### SCALA API ######
@@ -65,9 +60,14 @@ pushd ${DOC_DIR}
 	mkdir  ${MONGOEDS_DOCS_DIR}	
 	unzip -d ${MONGOEDS_DOCS_DIR} ${XAP_DIR}/docs/mongo-datasource.zip	
 
+	###### CASSANDRA API #####
+	if [ -f ${XAP_DIR}/docs/xap-cassandra.zip ]; then
+		mkdir  ${CASSANDRA_DOCS_DIR}	
+		unzip -d ${CASSANDRA_DOCS_DIR} ${XAP_DIR}/docs/xap-cassandra.zip
+	fi
 
 	###### REMOVE LINKS ######
-	rm ${JAVADOC_LINK} ${DOTNETDOC_LINK} ${CPPDOC_LINK} ${SCALADOC_LINK} ${MONGOEDSDOC_LINK}
+	rm ${JAVADOC_LINK} ${DOTNETDOC_LINK} ${CPPDOC_LINK} ${SCALADOC_LINK} ${MONGOEDSDOC_LINK} ${CASSANDRA_LINK}
 
 	###### CREATE LINKS ######
 	ln -s ${JAVA_DOCS_DIR} ${JAVADOC_LINK}
@@ -75,6 +75,10 @@ pushd ${DOC_DIR}
 	ln -s ${CPP_DOCS_DIR} ${CPPDOC_LINK}
 	ln -s ${SCALA_DOCS_DIR} ${SCALADOC_LINK}
 	ln -s ${MONGOEDS_DOCS_DIR} ${MONGOEDSDOC_LINK}
+	if [ -f ${XAP_DIR}/docs/xap-cassandra.zip ]; then
+		ln -s ${CASSANDRA_DOCS_DIR} ${CASSANDRA_LINK}
+	fi
+
 
 
 popd
