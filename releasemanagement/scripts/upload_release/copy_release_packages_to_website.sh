28e6f75b721844617a8d4771d3420217064e5b2c
limor-gs <limor@gigaspaces.com>
2015-02-05 13:54:47 +0200

    Update copy_release_packages_to_website.sh

--- releasemanagement/scripts/upload_release/copy_release_packages_to_website.sh
+++ releasemanagement/scripts/upload_release/copy_release_packages_to_website.sh
@@ -1,6 +1,16 @@
-#!/bMAJORin/bash
+#!/bin/bash
 
 source params.sh
+
+#create the lite packages
+pushd ${BUILD_DIR} 
+	mkdir xap-lite
+	pushd xap-lite	
+		cp -rp ../xap-premium/1.5/gigaspaces-xap-premium-* ../xap-premium/dotnet/GigaSpaces-XAP.NET-Premium-* .	
+		rename 's/premium/lite/' *.zip
+		rename 's/Premium/Lite/' *.msi
+	popd
+popd
 
 ssh  -i ~/.ssh/website  tempfiles@www.gigaspaces.com "mkdir -p ~/download_files/${MAJOR}/${VERSION}/"
 
@@ -9,7 +19,7 @@ ssh  -i ~/.ssh/website  tempfiles@www.gigaspaces.com "mkdir -p ~/download_files/
 
 for file in `find ${BUILD_DIR} -name "*.zip" -o -name "*.tar.gz" -o -name "*.msi" -o -name "*.rpm" | grep -v license | grep -v testsuite`; do
 
-  echo Uploading $file to ~/download_files/${MAJOR}/${VERSION}/
-  scp -i ~/.ssh/website $file tempfiles@www.gigaspaces.com:~/download_files/${MAJOR}/${VERSION}/
+  echo Uploading $file to ~/download_files/${MAJOR}/${VERSION}
+  scp -i ~/.ssh/website $file tempfiles@www.gigaspaces.com:~/download_files/${MAJOR}/${VERSION}
 
 done
