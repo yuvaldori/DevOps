25c62fc723b51bf8e168f2ce0d1e6422ac57b523
limor-gs <limor@gigaspaces.com>
2015-02-24 11:00:18 +0200

    Update travis.py

--- releasemanagement/scripts/cosmo/travis.py
+++ releasemanagement/scripts/cosmo/travis.py
@@ -74,7 +74,7 @@ for repo,sha in d.items():
 			branch_name=branch_name,
 			timeout_min=180)
 			for key,value in jobs_state.items():
-				#print(key, ":", value)
+				print(key, ":", value)
 				if value=='passed':
 					print key + ' success'
 				else:
