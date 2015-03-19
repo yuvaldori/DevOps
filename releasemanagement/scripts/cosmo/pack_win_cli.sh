948a54fb6ba49711c8a323891f17eb4b8d020785
limor-gs <limor@gigaspaces.com>
2015-03-19 12:09:30 +0200

    Update pack_win_cli.sh

--- releasemanagement/scripts/cosmo/pack_win_cli.sh
+++ releasemanagement/scripts/cosmo/pack_win_cli.sh
@@ -41,6 +41,9 @@ echo "ip_address="$ip_address
 ##copy windows exe file
 sudo mkdir -p /cloudify
 sudo chown tgrid -R /cloudify
+
+echo "sshpass -p '$WINDOWS_PASS' scp -p -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+Administrator@$ip_address:/home/Administrator/cloudify-cli-packager/packaging/windows/inno/Output/CloudifyCLI*.exe /cloudify"
 sshpass -p '$WINDOWS_PASS' scp -p -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null Administrator@$ip_address:/home/Administrator/cloudify-cli-packager/packaging/windows/inno/Output/CloudifyCLI*.exe /cloudify
 
 exit_on_error
