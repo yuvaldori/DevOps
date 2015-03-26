652aab8eda8f51ecf34af7ff67b72bd465773c5c
limor-gs <limor@gigaspaces.com>
2015-03-26 16:41:56 +0200

    Update edit_repo_revision.sh

--- releasemanagement/scripts/cosmo/edit_repo_revision.sh
+++ releasemanagement/scripts/cosmo/edit_repo_revision.sh
@@ -4,7 +4,6 @@ source generic_functions.sh
 
 #DSL_SHA=$(echo $DSL_SHA)
 
-
 win_agent_fileName="$cloudify_packager_dir/vagrant/windows-agent/provision.bat"
 sed -i "s/.*SET REST_CLIENT_SHA=.*/SET REST_CLIENT_SHA=$REST_CLIENT_SHA/g" $win_agent_fileName
 sed -i "s/.*SET COMMON_PLUGIN_SHA=.*/SET COMMON_PLUGIN_SHA=$COMMON_PLUGIN_SHA/g" $win_agent_fileName
@@ -28,8 +27,8 @@ sed -i "s/.*MANAGER_BLUEPRINTS_SHA=.*/MANAGER_BLUEPRINTS_SHA=$MANAGER_BLUEPRINTS
 sed -i "s/.*PACKAGER_SHA=.*/PACKAGER_SHA=$PACKAGER_SHA/g" $docker_file
 
 cli_win_fileName="cloudify-cli-packager/vagrant/windows/provision.bat"
-sed -i "s/.*SET CORE_TAG_NAME=.*/SET CORE_TAG_NAME=$CORE_TAG_NAME/g" $cli_win_fileName
-sed -i "s/.*SET PLUGINS_TAG_NAME=.*/SET PLUGINS_TAG_NAME=$PLUGINS_TAG_NAME/g" $cli_win_fileName
+sed -i "s/.*SET CORE_TAG_NAME=.*/SET CORE_TAG_NAME=$core_tag_name/g" $cli_win_fileName
+sed -i "s/.*SET PLUGINS_TAG_NAME=.*/SET PLUGINS_TAG_NAME=$plugins_tag_name/g" $cli_win_fileName
 vbox_file="$cloudify_packager_dir/image-builder/provision/common.sh"
 docker_file="$cloudify_packager_dir/vagrant/docker_images/provision.sh"
 debian_agent_file="cloudify-packager/vagrant/debian-jessie-agent/provision.sh"
