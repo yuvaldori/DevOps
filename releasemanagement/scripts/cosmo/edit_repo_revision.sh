46eaef6152ac23b19e4781ca8d0d32468480ffd5
limor-gs <limor@gigaspaces.com>
2015-03-30 15:07:45 +0300

    Remove sha ids

--- releasemanagement/scripts/cosmo/edit_repo_revision.sh
+++ releasemanagement/scripts/cosmo/edit_repo_revision.sh
@@ -2,49 +2,7 @@
 
 source generic_functions.sh
 
-DSL_SHA=$(echo $DSL_SHA)
-REST_CLIENT_SHA=$(echo $REST_CLIENT_SHA)
-CLI_SHA=$(echo $CLI_SHA)
-OS_PROVIDER_SHA=$(echo $OS_PROVIDER_SHA)
-COMMON_PLUGIN_SHA=$(echo $COMMON_PLUGIN_SHA)
-PACKMAN_SHA=$(echo $PACKMAN_SHA)
-MANAGER_SHA=$(echo $MANAGER_SHA)
-SCRIPTS_PLUGIN_SHA=$(echo $SCRIPTS_PLUGIN_SHA)
-DIAMOND_PLUGIN_SHA=$(echo $DIAMOND_PLUGIN_SHA)
-OS_PLUGIN_SHA=$(echo $OS_PLUGIN_SHA)
-FABRIC_PLUGIN_SHA=$(echo $FABRIC_PLUGIN_SHA)
-MANAGER_BLUEPRINTS_SHA=$(echo $MANAGER_BLUEPRINTS_SHA)
-PACKAGER_SHA=$(echo $PACKAGER_SHA)
-core_tag_name=$(echo $core_tag_name)
-plugins_tag_name=$(echo $plugins_tag_name)
-
-
-echo "DSL_SHA=$DSL_SHA"
-echo "REST_CLIENT_SHA=$REST_CLIENT_SHA"
-echo "CLI_SHA=$CLI_SHA"
-echo "OS_PROVIDER_SHA=$OS_PROVIDER_SHA"
-echo "COMMON_PLUGIN_SHA=$COMMON_PLUGIN_SHA"
-echo "PACKMAN_SHA=$PACKMAN_SHA"
-echo "SCRIPTS_PLUGIN_SHA=$SCRIPTS_PLUGIN_SHA"
-echo "DIAMOND_PLUGIN_SHA=$DIAMOND_PLUGIN_SHA"
-echo "OS_PLUGIN_SHA=$OS_PLUGIN_SHA"
-echo "FABRIC_PLUGIN_SHA=$FABRIC_PLUGIN_SHA"
-echo "MANAGER_BLUEPRINTS_SHA=$MANAGER_BLUEPRINTS_SHA"
-echo "PACKAGER_SHA=$PACKAGER_SHA"
-echo "cloudify_packager_dir=$cloudify_packager_dir"
-echo "core_tag_name=$core_tag_name"
-echo "plugins_tag_name=$plugins_tag_name"
-#edit the revision number in linux/provision.sh
-
-win_fileName="cloudify-cli-packager/vagrant/windows/provision.bat"
-sed -i "s/.*SET DSL_SHA=.*/SET DSL_SHA=$DSL_SHA/g" $win_fileName
-sed -i "s/.*SET REST_CLIENT_SHA=.*/SET REST_CLIENT_SHA=$REST_CLIENT_SHA/g" $win_fileName
-sed -i "s/.*SET CLI_SHA=.*/SET CLI_SHA=$CLI_SHA/g" $win_fileName
-sed -i "s/.*SET OS_PROVIDER_SHA=.*/SET OS_PROVIDER_SHA=$OS_PROVIDER_SHA/g" $win_fileName
-sed -i "s/.*SET COMMON_PLUGIN_SHA=.*/SET COMMON_PLUGIN_SHA=$COMMON_PLUGIN_SHA/g" $win_fileName
-sed -i "s/.*SET OS_PLUGIN_SHA=.*/SET OS_PLUGIN_SHA=$OS_PLUGIN_SHA/g" $win_fileName
-sed -i "s/.*SET FABRIC_PLUGIN_SHA=.*/SET FABRIC_PLUGIN_SHA=$FABRIC_PLUGIN_SHA/g" $win_fileName
-sed -i "s/.*SET SCRIPTS_PLUGIN_SHA=.*/SET SCRIPTS_PLUGIN_SHA=$SCRIPTS_PLUGIN_SHA/g" $win_fileName
+#DSL_SHA=$(echo $DSL_SHA)
 
 win_agent_fileName="$cloudify_packager_dir/vagrant/windows-agent/provision.bat"
 sed -i "s/.*SET REST_CLIENT_SHA=.*/SET REST_CLIENT_SHA=$REST_CLIENT_SHA/g" $win_agent_fileName
@@ -52,14 +10,8 @@ sed -i "s/.*SET COMMON_PLUGIN_SHA=.*/SET COMMON_PLUGIN_SHA=$COMMON_PLUGIN_SHA/g"
 sed -i "s/.*SET MANAGER_SHA=.*/SET MANAGER_SHA=$MANAGER_SHA/g" $win_agent_fileName
 sed -i "s/.*SET SCRIPTS_PLUGIN_SHA=.*/SET SCRIPTS_PLUGIN_SHA=$SCRIPTS_PLUGIN_SHA/g" $win_agent_fileName
 sed -i "s/.*SET DIAMOND_PLUGIN_SHA=.*/SET DIAMOND_PLUGIN_SHA=$DIAMOND_PLUGIN_SHA/g" $win_agent_fileName
-
-fileName="cloudify-cli-packager/vagrant/linux/provision.sh"
-components_fileName="$cloudify_packager_dir/vagrant/cloudify-components/provision.sh"
-centos_agent_fileName="$cloudify_packager_dir/vagrant/centos-agent/provision.sh"
 ubuntu_agent_p_fileName="$cloudify_packager_dir/vagrant/ubuntu-precise-agent/provision.sh"
 ubuntu_agent_t_fileName="$cloudify_packager_dir/vagrant/ubuntu-trusty-agent/provision.sh"
-vbox_fileName="$cloudify_packager_dir/image-builder/provision/common.sh"
-docker_file="$cloudify_packager_dir/vagrant/docker_images/provision.sh"
 sed -i "s/.*REST_CLIENT_SHA=.*/REST_CLIENT_SHA=$REST_CLIENT_SHA/g" $centos_agent_fileName $ubuntu_agent_p_fileName $ubuntu_agent_t_fileName $fileName $vbox_fileName
 sed -i "s/.*COMMON_PLUGIN_SHA=.*/COMMON_PLUGIN_SHA=$COMMON_PLUGIN_SHA/g" $centos_agent_fileName $ubuntu_agent_p_fileName $ubuntu_agent_t_fileName $fileName $vbox_fileName
 sed -i "s/.*MANAGER_SHA=.*/MANAGER_SHA=$MANAGER_SHA/g" $centos_agent_fileName $ubuntu_agent_p_fileName $ubuntu_agent_t_fileName
@@ -74,7 +26,11 @@ sed -i "s/.*FABRIC_PLUGIN_SHA=.*/FABRIC_PLUGIN_SHA=$FABRIC_PLUGIN_SHA/g" $fileNa
 sed -i "s/.*MANAGER_BLUEPRINTS_SHA=.*/MANAGER_BLUEPRINTS_SHA=$MANAGER_BLUEPRINTS_SHA/g" $vbox_fileName
 sed -i "s/.*PACKAGER_SHA=.*/PACKAGER_SHA=$PACKAGER_SHA/g" $docker_file
 
+cli_win_fileName="cloudify-cli-packager/vagrant/windows/provision.bat"
+sed -i "s/.*SET CORE_TAG_NAME=.*/SET CORE_TAG_NAME=$core_tag_name/g" $cli_win_fileName
+sed -i "s/.*SET PLUGINS_TAG_NAME=.*/SET PLUGINS_TAG_NAME=$plugins_tag_name/g" $cli_win_fileName
+vbox_file="$cloudify_packager_dir/image-builder/provision/common.sh"
+docker_file="$cloudify_packager_dir/vagrant/docker_images/provision.sh"
 debian_agent_file="cloudify-packager/vagrant/debian-jessie-agent/provision.sh"
-sed -i "s/.*core_tag_name=.*/core_tag_name=$core_tag_name/g" $debian_agent_file
-sed -i "s/.*plugins_tag_name=.*/plugins_tag_name=$plugins_tag_name/g" $debian_agent_file
-
+sed -i "s/.*CORE_TAG_NAME=.*/CORE_TAG_NAME=$core_tag_name/g" $debian_agent_file $vbox_file $docker_file
+sed -i "s/.*PLUGINS_TAG_NAME=.*/PLUGINS_TAG_NAME=$plugins_tag_name/g" $debian_agent_file $vbox_file $docker_file
