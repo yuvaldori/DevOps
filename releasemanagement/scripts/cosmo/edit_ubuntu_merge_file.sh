4478a5ed8f7c78103ff6d2ddf6af6761d21d4578
limor-gs <limor@gigaspaces.com>
2015-04-16 16:23:11 +0300

    Update edit_ubuntu_merge_file.sh

--- releasemanagement/scripts/cosmo/edit_ubuntu_merge_file.sh
+++ releasemanagement/scripts/cosmo/edit_ubuntu_merge_file.sh
@@ -6,8 +6,8 @@ url_prefix="http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/"$MAJ
 if [ "$TRUSTY_TYPE" == "commercial" ]
 then
   ubuntu_agent_trusty_commercial_url=$url_prefix"/cloudify-ubuntu-trusty-commercial-agent_"$PRODUCT_VERSION_FULL"_amd64.deb"
-  sed -i "s|.*cloudify-ubuntu-trusty-agent.*|curl $(echo ${ubuntu_agent_trusty_commercial_url}) --create-dirs -o /opt/tmp/manager/ubuntu_trusty_agent.deb \&\& \\\|" $docker_ubuntu_merge_file
+  sed -i "s|.*cloudify-ubuntu-trusty.*|curl $(echo ${ubuntu_agent_trusty_commercial_url}) --create-dirs -o /opt/tmp/manager/ubuntu_trusty_agent.deb \&\& \\\|" $docker_ubuntu_merge_file
 else
   ubuntu_agent_trusty_url=$url_prefix"/cloudify-ubuntu-trusty-agent_"$PRODUCT_VERSION_FULL"_amd64.deb"
-  sed -i "s|.*cloudify-ubuntu-trusty-agent.*|curl $(echo ${ubuntu_agent_trusty_url}) --create-dirs -o /opt/tmp/manager/ubuntu_trusty_agent.deb \&\& \\\|" $docker_ubuntu_merge_file
+  sed -i "s|.*cloudify-ubuntu-trusty.*|curl $(echo ${ubuntu_agent_trusty_url}) --create-dirs -o /opt/tmp/manager/ubuntu_trusty_agent.deb \&\& \\\|" $docker_ubuntu_merge_file
 fi
