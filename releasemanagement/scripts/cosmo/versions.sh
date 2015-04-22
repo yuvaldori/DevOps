8ba426238f0fc446c98c4831d9cc5269eb8b7b4b
limor-gs <limor@gigaspaces.com>
2015-04-22 13:43:49 +0300

    CFY-2552 Default Docker image in manager blueprints should be commercial

--- releasemanagement/scripts/cosmo/versions.sh
+++ releasemanagement/scripts/cosmo/versions.sh
@@ -125,7 +125,7 @@ then
 	ubuntu_agent_url=$url_prefix"/cloudify-ubuntu-agent_"$PRODUCT_VERSION_FULL"_amd64.deb"
 	centos_agent_url=$url_prefix"/cloudify-centos-final-agent_"$PRODUCT_VERSION_FULL"_amd64.deb"
 	windows_agent_url=$url_prefix"/cloudify-windows-agent_"$PRODUCT_VERSION_FULL"_amd64.deb"
-	docker_url=$url_prefix"/cloudify-docker_"$PRODUCT_VERSION_FULL".tar"
+	docker_url=$url_prefix"/cloudify-docker-commercial_"$PRODUCT_VERSION_FULL".tar"
 	
 	#sed -i "s|ui_package_url.*|ui_package_url: $(echo ${ui_package_url})|g" $defaults_config_yaml_file $config_yaml_file $defaults_libcloud_config_yaml_file $config_libcloud_yaml_file $blueprints_aws_ec2_yaml $blueprints_cloudstack_yaml $blueprints_nova_net_yaml $blueprints_openstack_yaml $blueprints_simple_yaml $blueprints_softlayer_yaml $blueprints_vsphere_yaml
 	sed -i "s|ubuntu_agent_url.*|ubuntu_agent_url: $(echo ${ubuntu_agent_url})|g" $blueprints_aws_ec2_yaml $blueprints_cloudstack_yaml $blueprints_nova_net_yaml $blueprints_openstack_yaml $blueprints_simple_yaml $blueprints_softlayer_yaml $blueprints_vsphere_yaml
