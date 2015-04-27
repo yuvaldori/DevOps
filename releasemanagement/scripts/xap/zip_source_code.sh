7fc7059c6316b3ea152b2f4d827d97a8230eb7f4
limor-gs <limor@gigaspaces.com>
2015-04-27 17:40:58 +0300

    Create zip_source_code.sh

---
+++ releasemanagement/scripts/xap/zip_source_code.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+
+function  exit_on_error {
+      status=$?
+      echo "exit code="$status    
+      if [ $status != 0 ] ; then
+         	echo "Failed (exit code $status)" 
+		exit 1
+      fi
+
+}
+
+temp_branch_name="release_tag"
+tag_name="10.1.0_ga_build12600_03_30_2015"
+
+for dir in `pwd`/*/
+do
+    dir=${dir%*/}
+    repo=${dir##*/}
+        
+      if [ "$repo" != "examples" ] && [ "$repo" != "xap-session-sharing-manager-itests" ] && [ "$repo" != "xap-spring-data" ] &&  [[ ! "$repo" =~ "xap-example" ]]
+	    then
+		      echo "### Processing repository: $repo"
+		      pushd $repo
+		        git checkout master
+			      git pull origin master
+			      git branch | grep $temp_branch_name && git branch -D $temp_branch_name
+			      
+			      git checkout -b $temp_branch_name tags/$tag_name
+			      exit_on_error
+			      
+			      git —no-pager log -1
+			      
+			    popd
+			fi
+done
+
+zip -r 10.1.0_source_code.zip ./* -x *.sh repo_names_sha devops/* examples/* xap-session-sharing-manager-itests/* xap-spring-data/* xap-example-data/* xap-example-helloworld/* xap-example-tutorials/*  xap-example-web/*
