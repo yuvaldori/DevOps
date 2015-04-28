b0cbbddf413a88c728b75968a889050b20d1442d
limor-gs <limor@gigaspaces.com>
2015-04-28 18:55:15 +0300

    Update versions.sh

--- releasemanagement/scripts/cosmo/versions.sh
+++ releasemanagement/scripts/cosmo/versions.sh
@@ -263,16 +263,16 @@ do
  			exit_on_error
  			
  			curl -u opencm:$GITHUB_PASSWORD -L https://github.com/cloudify-cosmo/cloudify-manager-blueprints/archive/temp_branch.tar.gz > ../cloudify-manager-blueprints.tar.gz
- 			git checkout -
- 			if [[ `git branch | grep temp_branch` ]]
- 			then
- 				git branch -D temp_branch
- 				exit_on_error
- 			elif [[ `git branch -r | grep origin/temp_branch` ]]
- 			then
- 				git push origin --delete temp_branch
- 				exit_on_error
- 			fi
+ 			#git checkout -
+ 			#if [[ `git branch | grep temp_branch` ]]
+ 			#then
+ 			#	git branch -D temp_branch
+ 			#	exit_on_error
+ 			#elif [[ `git branch -r | grep origin/temp_branch` ]]
+ 			#then
+ 			#	git push origin --delete temp_branch
+ 			#	exit_on_error
+ 			#fi
                 fi
  		
  		echo "Updating VERSION file (cloudify-cli/cloudify-ui)"
