16ea17dba69f38c3f1f37b8046a2f181230be92d
opencm <opencm@gigaspaces.com>
2015-05-07 14:09:11 +0300

    Create rename_branch.sh

---
+++ releasemanagement/scripts/allproj/rename_branch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function  exit_on_error {
+    status=$?
+    echo "exit code="$status    
+    if [ $status != 0 ] ; then
+        echo "Failed (exit code $status)" 
+        #exit 1
+    fi
+
+}
+
+for dir in `pwd`/*/
+do
+      dir=${dir%*/}
+      repo=${dir##*/}
+
+          echo "### Processing repository: $repo"
+          pushd $repo
+            if [ -d ".git" ]
+            then
+                for build_branch in ${BUILD_BRANCHES_LIST}
+                do
+
+                        if [[ `git branch -r | grep origin/$build_branch` ]]
+                        then
+                            echo "Removing $build_branch"
+                            git branch -D $build_branch
+                            git push origin --delete $build_branch
+                            exit_on_error
+                        fi
+                done
+            fi
+      popd
+done
