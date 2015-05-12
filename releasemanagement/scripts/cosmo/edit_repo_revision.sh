4b078b00568ff0f8e2f149b2f0de44ebb307cebd
opencm <opencm@gigaspaces.com>
2015-05-12 15:53:07 +0300

    Update edit_repo_revision.sh

--- releasemanagement/scripts/cosmo/edit_repo_revision.sh
+++ releasemanagement/scripts/cosmo/edit_repo_revision.sh
@@ -13,5 +13,6 @@ linux_agent_file="$cloudify_packager_dir/vagrant/agents/provision.sh"
 linux_cli_file="$cloudify_packager_dir/vagrant/cli/provision.sh"
 vbox_file="cloudify-packager/image-builder/quickstart-vagrantbox/provision/common.sh"
 docker_file="$cloudify_packager_dir/vagrant/docker_images/provision.sh"
-sed -i "s/.*CORE_TAG_NAME=.*/CORE_TAG_NAME=$core_tag_name/g" $linux_agent_file $vbox_file $docker_file $linux_cli_file
+ubuntu_merge_file="cloudify-packager/docker/ubuntu_agent/scripts/install_packman.sh"
+sed -i "s/.*CORE_TAG_NAME=.*/CORE_TAG_NAME=$core_tag_name/g" $linux_agent_file $vbox_file $docker_file $linux_cli_file $ubuntu_merge_file
 sed -i "s/.*PLUGINS_TAG_NAME=.*/PLUGINS_TAG_NAME=$plugins_tag_name/g" $linux_agent_file $vbox_file $docker_file $linux_cli_file
