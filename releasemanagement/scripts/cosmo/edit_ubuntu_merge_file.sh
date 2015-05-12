b59efde562320c3c01344e05870ef81f56eb5a0c
opencm <opencm@gigaspaces.com>
2015-05-12 13:11:18 +0300

    Update edit_ubuntu_merge_file.sh

--- releasemanagement/scripts/cosmo/edit_ubuntu_merge_file.sh
+++ releasemanagement/scripts/cosmo/edit_ubuntu_merge_file.sh
@@ -9,7 +9,7 @@ then
   ubuntu_agent_precise_commercial_url=$url_prefix"/cloudify-ubuntu-precise-commercial-agent_"$PRODUCT_VERSION_FULL"_amd64.deb"
   sed -i "s|.*cloudify-ubuntu-trusty.*|curl $(echo ${ubuntu_agent_trusty_commercial_url}) --create-dirs -o /opt/tmp/manager/ubuntu_trusty_agent.deb \&\& \\\|" $docker_ubuntu_merge_file
   sed -i "s|.*cloudify-ubuntu-precise.*|curl $(echo ${ubuntu_agent_precise_commercial_url}) --create-dirs -o /opt/tmp/manager/ubuntu_precise_agent.deb \&\& \\\|" $docker_ubuntu_merge_file
-  sed -i "s|.*pkm pack -c.*|pkm pack -c cloudify-ubuntu-trusty-commercial-agent|" $docker_ubuntu_merge_file
+  sed -i "s|.*pkm pack -c.*|pkm pack -c cloudify-ubuntu-commercial-agent|" $docker_ubuntu_merge_file
 else
   ubuntu_agent_trusty_url=$url_prefix"/cloudify-ubuntu-trusty-agent_"$PRODUCT_VERSION_FULL"_amd64.deb"
   ubuntu_agent_precise_url=$url_prefix"/cloudify-ubuntu-precise-agent_"$PRODUCT_VERSION_FULL"_amd64.deb"
