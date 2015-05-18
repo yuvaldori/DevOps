a12ce5519c068a6dde2217db8008e30162058f7f
limor-gs <limor@gigaspaces.com>
2015-05-18 11:53:04 +0300

    Update versions.sh

--- releasemanagement/scripts/cosmo/versions.sh
+++ releasemanagement/scripts/cosmo/versions.sh
@@ -291,6 +291,7 @@ do
 		        then
 		        	echo "Copy cli VERSION file to cloudify-packager folder"
 		        	cp VERSION ../../cloudify-packager
+		        	cp VERSION ../../cloudify-packager/vagrant/cli/windows
 		                popd
 		        fi
 		fi
