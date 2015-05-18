831951764b2c97cb99d79ac50abc3ab97424fd96
limor-gs <limor@gigaspaces.com>
2015-05-18 15:56:34 +0300

    Update pack_cli.sh

--- releasemanagement/scripts/cosmo/pack_cli.sh
+++ releasemanagement/scripts/cosmo/pack_cli.sh
@@ -16,7 +16,8 @@ function  exit_on_error {
       if [ $status != 0 ] ; then
          	echo "Failed (exit code $status)"
          	vagrant destroy -f centos6_5_final_cli_aws
-		vagrant destroy -f centos7_0_final_cli_aws            
+		vagrant destroy -f centos7_0_final_cli_aws
+		vagrant destroy -f windows_aws
 		exit 1
       fi
 
@@ -27,6 +28,7 @@ sudo chown tgrid -R /cloudify
 #destroy centos machines
 vagrant destroy -f centos6_5_final_cli_aws
 vagrant destroy -f centos7_0_final_cli_aws
+vagrant destroy -f windows_aws
 
 #centos6_5_final_cli_aws
 (vagrant up centos6_5_final_cli_aws --provider=aws && 
@@ -35,7 +37,7 @@ vagrant destroy -f centos7_0_final_cli_aws
    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
    -i ~/.ssh/aws/vagrant_build.pem \
    root@$centos65_ip:/cloudify/*.rpm /cloudify/cloudify-linux_cli_centos6_5.rpm && exit_on_error) &
-
+sleep 5
 #centos7_0_final_cli_aws
 (vagrant up centos7_0_final_cli_aws --provider=aws && 
  centos7_ip=`vagrant ssh-config centos7_0_final_cli_aws | grep HostName | sed "s/HostName//g" | sed "s/ //g"` && 
@@ -43,9 +45,18 @@ vagrant destroy -f centos7_0_final_cli_aws
    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
    -i ~/.ssh/aws/vagrant_build.pem \
    ec2-user@$centos7_ip:/cloudify/*.rpm /cloudify/cloudify-linux_cli_centos7.rpm && exit_on_error) &
+ sleep 5  
+  #windows_aws
+(vagrant up windows_aws --provider=aws && 
+ win_ip=`vagrant ssh-config windows_aws | grep HostName | sed "s/HostName//g" | sed "s/ //g"` && 
+   echo "win_ip=win_ip" && 
+   scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+   -i ~/.ssh/aws/vagrant_build.pem \
+   Administrator@$win_ip:/home/Administrator/packaging/output/*.exe /cloudify/ && exit_on_error) &
 
 wait
 
 #destroy centos machines
 vagrant destroy -f centos6_5_final_cli_aws
 vagrant destroy -f centos7_0_final_cli_aws
+vagrant destroy -f windows_aws
