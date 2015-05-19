04ac7ae91620e493f91613e84ae28f81681aff53
limor-gs <limor@gigaspaces.com>
2015-05-19 13:58:14 +0300

    Update edit_repo_revision.sh

--- releasemanagement/scripts/cosmo/edit_repo_revision.sh
+++ releasemanagement/scripts/cosmo/edit_repo_revision.sh
@@ -5,10 +5,8 @@ source generic_functions.sh
 #DSL_SHA=$(echo $DSL_SHA)
 
 win_agent_fileName="$cloudify_packager_dir/vagrant/agents/provision.bat"
-cli_win_fileName="cloudify-cli-packager/vagrant/windows/provision.bat"
-cli_win_fileName1="cloudify-packager/vagrant/cli/provision.bat"
-sed -i "s/.*SET CORE_TAG_NAME=.*/SET CORE_TAG_NAME=$core_tag_name/g" $cli_win_fileName $win_agent_fileName $cli_win_fileName1
-sed -i "s/.*SET PLUGINS_TAG_NAME=.*/SET PLUGINS_TAG_NAME=$plugins_tag_name/g" $cli_win_fileName $win_agent_fileName $cli_win_fileName1
+sed -i "s/.*SET CORE_TAG_NAME=.*/SET CORE_TAG_NAME=$core_tag_name/g" $win_agent_fileName
+sed -i "s/.*SET PLUGINS_TAG_NAME=.*/SET PLUGINS_TAG_NAME=$plugins_tag_name/g" $win_agent_fileName
 linux_agent_file="$cloudify_packager_dir/vagrant/agents/provision.sh"
 linux_cli_file="$cloudify_packager_dir/vagrant/cli/provision.sh"
 vbox_file="cloudify-packager/image-builder/quickstart-vagrantbox/provision/common.sh"
