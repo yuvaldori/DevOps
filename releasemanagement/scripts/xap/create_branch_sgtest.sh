812ca84564bb31075c1f3d8fbd5b343e934d312d
limor-gs <limor@gigaspaces.com>
2015-05-20 11:55:10 +0300

    Update create_branch_sgtest.sh

--- releasemanagement/scripts/xap/create_branch_sgtest.sh
+++ releasemanagement/scripts/xap/create_branch_sgtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-XAP_TRUNK_SVN_URL="svn://pc-lab14/SVN/xap/trunk/quality"
+XAP_TRUNK_SVN_URL="svn://pc-lab14/SVN/xap"
 BRANCH_FOLDER="10_2_X"
 BRANCH_NAME="10.2.0m2-build"
 SVN_REVISION_NUMBER=`svn info ${XAP_TRUNK_SVN_URL} | grep Revision | awk '{print $2}'`
-svn cp ${XAP_TRUNK_SVN_URL} svn://svn-srv/SVN/xap/branches/${BRANCH_FOLDER}/${BRANCH_NAME} -m "Create branch ${BRANCH_NAME} from ${XAP_TRUNK_SVN_URL}, revision: ${SVN_REVISION_NUMBER}"
+svn cp ${XAP_TRUNK_SVN_URL}/trunk/quality ${XAP_TRUNK_SVN_URL}/branches/${BRANCH_FOLDER}/${BRANCH_NAME} -m "Create branch ${BRANCH_NAME} from ${XAP_TRUNK_SVN_URL}, revision: ${SVN_REVISION_NUMBER}"
