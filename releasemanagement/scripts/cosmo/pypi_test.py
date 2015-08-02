5bd6c62fb6a1ce51516bf1b5394564f0e8206d34
limor-gs <limor@gigaspaces.com>
2015-08-02 16:19:18 +0300

    remove cloudify-agent-packager

--- releasemanagement/scripts/cosmo/pypi_test.py
+++ releasemanagement/scripts/cosmo/pypi_test.py
@@ -6,7 +6,7 @@ import pip
 
 def test():
         print "### Install modules from pypi"
-        modules = ['cloudify', 'cloudify-diamond-plugin', 'cloudify-agent-packager']
+        modules = ['cloudify', 'cloudify-diamond-plugin']
         for module in modules:
                 pip.main(['install', '--pre', '{0}'.format(module)])
         pip freeze
