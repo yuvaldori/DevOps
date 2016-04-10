f4dd5b70377e3a13f0f4b8244e3988c57e1493d9
limor-gs <limor@gigaspaces.com>
2016-04-10 15:14:19 +0300

    Update copy_milestone_packages_to_website.sh

--- releasemanagement/scripts/upload_release/copy_milestone_packages_to_website.sh
+++ releasemanagement/scripts/upload_release/copy_milestone_packages_to_website.sh
@@ -2,37 +2,9 @@
 
 source params.sh
 
-##copy gslicense.xml from xap-premium zip to xap-bigdata
-#cp -rp ${BUILD_DIR} ${BUILD_DIR}_backup
-#pushd ${BUILD_DIR}/xap-premium/1.5
-#    PREMIUM_ZIP=`find . -name "*-with-license.zip"`
-#    echo "PREMIUM_ZIP="${PREMIUM_ZIP}
-#    PREMIUM_UNZIP_DEST=`echo ${PREMIUM_ZIP}| sed 's/\.zip//g'`
-#    echo "PREMIUM_UNZIP_DEST="${PREMIUM_UNZIP_DEST}
-#    unzip ${PREMIUM_ZIP} -d ${PREMIUM_UNZIP_DEST}
-#popd
-#pushd ${BUILD_DIR}/xap-bigdata/1.5
-#   BIGDATA_ZIP=`find . -name "gigaspaces-xap-premium-*.zip"`
-#   echo "BIGDATA_ZIP="${BIGDATA_ZIP} 
-#   BIGDATA_UNZIP_DEST=`echo ${BIGDATA_ZIP}| sed 's/\.zip//g'`
-#   echo "BIGDATA_UNZIP_DEST="${BIGDATA_UNZIP_DEST}
-#   MAIN_ZIP_DIR=gigaspaces-xap-premium-${VERSION}-${MILESTONE}
-#   mkdir ${MAIN_ZIP_DIR}
-#   cp ${BUILD_DIR}/xap-premium/1.5/${PREMIUM_UNZIP_DEST}/gigaspaces-xap-premium*/gslicense.xml ${MAIN_ZIP_DIR}/
-#   zip ${BIGDATA_ZIP} ${MAIN_ZIP_DIR}/gslicense.xml
-#   rm -rf ${MAIN_ZIP_DIR}
-#popd
-#pushd ${BUILD_DIR}/xap-premium/1.5   
-#   rm -rf ${PREMIUM_UNZIP_DEST}
-#popd
-
-#limor 10.0.0 rc - copy updated gslicense to premium zip file or we should upload the zip with license 
-#zip gigaspaces-xap-premium-10.0.0-rc-b11510.zip gigaspaces-xap-premium-10.0.0-rc/gslicense.xml
-
 #upload package files to website
 ssh  -i ~/.ssh/website  tempfiles@www.gigaspaces.com "mkdir -p ~/tempfiles/downloads/EarlyAccess/xap/${VERSION}/${MILESTONE}/"
 
-#for file in `find ${BUILD_DIR} -name "*.zip" -o -name "*.tar.gz" -o -name "*.msi"  | grep -v license | grep -v testsuite | grep -v xap-premium.1.5`; do
 for file in `find ${BUILD_DIR} -name "*.zip" -o -name "*.tar.gz" -o -name "*.msi" -o -name "*.rpm"  | grep -v testsuite`; do
 
   echo Uploading $file to ~/tempfiles/downloads/EarlyAccess/xap/${VERSION}/${MILESTONE}
